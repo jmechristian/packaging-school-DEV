c3fb94793c5f90f2834f506c0ddf6fd3ba415395
jmechristian <jmechristian@gmail.com>
2022-09-29 18:39:00 -0400

    sticky

--- slices/CertMenu/index.js
+++ slices/CertMenu/index.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
-import { motion } from 'framer-motion';
+import { motion, useInView } from 'framer-motion';
 
 const CertMenu = ({ slice }) => {
   const [contentMenuOpen, setContentMenuOpen] = useState(false);
+
+  const menuRef = useRef();
+
+  const inView = useInView(menuRef);
+
+  // useEffect(() => {
+  //   if (inView) {
+  //     console.log('In View');
+  //   } else {
+  //     console.log('Not In View');
+  //   }
+  // }, [inView]);
 
   const menuVariants = {
     open: {
@@ -23,41 +35,46 @@ const CertMenu = ({ slice }) => {
   };
 
   return (
-    <section className='container__inner flex relative w-full justify-between items-center mt-3 lg:mt-5'>
-      <div className=' font-medium flex lg:hidden'>
-        <motion.div
-          className='flex flex-col gap-3 absolute top-8 left-4 md:left-7 bg-white px-4 py-4 drop-shadow-lg'
-          variants={menuVariants}
-          initial={false}
-          animate={contentMenuOpen ? 'open' : 'closed'}
-        >
+    <div className='w-full bg-white sticky top-0 '>
+      <motion.section
+        className='container__inner flex w-full justify-between items-center py-4 relative'
+        ref={menuRef}
+      >
+        <div className=' font-medium flex lg:hidden'>
+          <motion.div
+            className='flex flex-col gap-3 absolute top-12 left-4 md:left-7 bg-white px-4 py-4 drop-shadow-lg'
+            variants={menuVariants}
+            initial={false}
+            animate={contentMenuOpen ? 'open' : 'closed'}
+          >
+            {slice?.items?.map((item, i) => (
+              <span className='first:text-base-brand' key={i}>
+                {item.link_name}
+              </span>
+            ))}
+          </motion.div>
+          <div
+            className='flex text-base-brand gap-2 items-center'
+            onClick={() => setContentMenuOpen(!contentMenuOpen)}
+          >
+            Jump To{' '}
+            <ChevronDownIcon className='w-5 h-5 stroke-base-brand cursor-pointer' />
+          </div>
+        </div>
+        <div className='hidden lg:flex gap-6 font-medium'>
           {slice?.items?.map((item, i) => (
             <span className='first:text-base-brand' key={i}>
               {item.link_name}
             </span>
           ))}
-        </motion.div>
-        <div
-          className='flex text-base-brand gap-2 items-center'
-          onClick={() => setContentMenuOpen(!contentMenuOpen)}
-        >
-          Jump To{' '}
-          <ChevronDownIcon className='w-5 h-5 stroke-base-brand cursor-pointer' />
         </div>
-      </div>
-      <div className='hidden lg:flex gap-6 font-medium'>
-        {slice?.items?.map((item, i) => (
-          <span className='first:text-base-brand' key={i}>
-            {item.link_name}
-          </span>
-        ))}
-      </div>
-      <div className='rounded'>
-        <div className='bg-base-dark text-white text-sm px-4 py-2 rounded'>
-          Enroll Now
+        <div className='rounded'>
+          <div className='bg-base-dark text-white text-sm px-4 py-2 rounded'>
+            Enroll Now
+          </div>
         </div>
-      </div>
-    </section>
+      </motion.section>
+    </div>
   );
 };
 
