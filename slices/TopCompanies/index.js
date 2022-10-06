cd1f3adc97e48eee9f3baa68876810e311260d9d
jmechristian <jmechristian@gmail.com>
2022-10-06 10:01:42 -0400

    carousel

--- slices/TopCompanies/index.js
+++ slices/TopCompanies/index.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import { PrismicRichText } from '@prismicio/react';
+import React, { useEffect, useState, useRef } from 'react';
+import { motion } from 'framer-motion';
 
-const TopCompanies = ({ slice }) => (
-  <section className='bg-base-dark container__inner md:rounded-lg'>
-    <div className='flex flex-col py-10 px-6 gap-6'>
-      <div className='text-center font-semibold text-white text-xl'>
-        Top companies choose the{' '}
-        <span className='text-clemson'>Packaging School</span> to build
-        in-demand skills.
+const TopCompanies = ({ slice }) => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    setWidth(carousel.current.offsetWidth - carousel.current.scrollWidth);
+  }, []);
+
+  return (
+    <section className='lg:container__inner'>
+      <div className='bg-base-dark lg:rounded py-12 w-full'>
+        <div className='flex flex-col w-4/5 mx-auto'>
+          <div className='text-center text-white text-2xl font-semibold'>
+            Top Companies Trust{' '}
+            <span className='text-clemson'>The Packaging School</span> for
+            On-Demand Skills
+          </div>
+          <div className='overflow-hidden cursor-pointer' ref={carousel}>
+            <motion.div
+              drag='x'
+              dragConstraints={{
+                right: 0,
+                left: width,
+              }}
+              className='flex'
+            >
+              {slice?.items?.map((item, i) => (
+                <motion.div key={i} className='p-8 min-w-max min-h-max'>
+                  <img
+                    src={item.logo.url}
+                    alt={item.logo.alt}
+                    className='w-full h-full pointer-events-none'
+                  />
+                </motion.div>
+              ))}
+            </motion.div>
+          </div>
+        </div>
       </div>
-      <div className='flex flex-row justify-center flex-nowrap overflow-auto gap-16'>
-        {slice?.items?.map((item, i) => (
-          <img
-            src={item.logo.url}
-            alt={item.logo.alt}
-            className='w-28'
-            key={i}
-          />
-        ))}
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default TopCompanies;
