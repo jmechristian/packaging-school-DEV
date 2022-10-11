9a24c0ecd66d8b7ae609ab24edf747a61c7f35af
jmechristian <jmechristian@gmail.com>
2022-10-11 11:24:15 -0400

    scroll indicator

--- components/courses/RelatedCourses.js
+++ components/courses/RelatedCourses.js
@@ -1,16 +1,19 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 import RelatedCourse from './RelatedCourse';
 import { related } from '../../dummyData';
 
 const RelatedCourses = () => {
   const [width, setWidth] = useState(0);
+  const relatedRef = useRef(null);
 
   useEffect(() => {
     setWidth(relatedRef.current.offsetWidth - relatedRef.current.scrollWidth);
   }, []);
 
-  const relatedRef = useRef();
+  const dragX = useMotionValue(0);
+  const indicatorWidth = useTransform(dragX, [0, width], ['0%', '100%']);
+
   return (
     <motion.div
       className='mt-8 flex flex-col gap-6 overflow-hidden'
@@ -21,6 +24,7 @@ const RelatedCourses = () => {
         drag='x'
         dragConstraints={{ right: 0, left: width }}
         dragTransition={{ power: 0.3 }}
+        style={{ x: dragX }}
       >
         {related.map((item, i) => (
           <motion.div key={i} className='w-full'>
@@ -28,7 +32,14 @@ const RelatedCourses = () => {
           </motion.div>
         ))}
       </motion.div>
-      <div className='flex flex-row gap-2 justify-center'></div>
+      <div className='flex flex-row gap-2 justify-center'>
+        <motion.div className='h-1.5 bg-gray-300 relative w-1/2 mt-2'>
+          <motion.div
+            className='absolute  bg-gradient-to-r from-base-light via-base-mid to-base-dark h-16 z-20'
+            style={{ width: indicatorWidth }}
+          ></motion.div>
+        </motion.div>
+      </div>
     </motion.div>
   );
 };
