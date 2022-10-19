da4054577a18c5573a6872be61aa44a3e47d7f41
jmechristian <jmechristian@gmail.com>
2022-10-19 12:59:28 -0400

    added video to cert

--- .slicemachine/assets/slices/CertificateHero/index.stories.js
+++ .slicemachine/assets/slices/CertificateHero/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"headline":[{"type":"heading1","text":"Visitor","spans":[]}],"subheadline":[{"type":"paragraph","text":"Sint ad esse proident ex cillum consequat magna labore ea anim magna deserunt.","spans":[]}],"hero_image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1496181133206-80ce9b88a853"},"cta_headline":"adult"},"slice_type":"certificate_hero","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"headline":[{"type":"heading1","text":"Teach","spans":[]}],"subheadline":[{"type":"paragraph","text":"Cillum dolor in reprehenderit mollit sit commodo reprehenderit exercitation laboris ullamco reprehenderit ea cupidatat. Laborum occaecat tempor occaecat in enim in commodo.","spans":[]}],"hero_image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1587840171670-8b850147754e"},"cta_headline":"eleven","youtube_id":"glad"},"slice_type":"certificate_hero","id":"_Default"}} />
 _Default.storyName = ''
--- slices/CertificateHero/index.js
+++ slices/CertificateHero/index.js
@@ -1,69 +1,25 @@
 import React from 'react';
 import { PrismicRichText } from '@prismicio/react';
-import CTAButtonLarge from '../../components/CTAButtonLarge';
+import CTACountdown from './components/CTACountdown';
+import YouTubeEmbed from '../../components/YouTubeEmbed';
 
 const CertificateHero = ({ slice }) => (
   <section className='bg-dark'>
-    <div className='container__inner hero__padding flex flex-col'>
-      <div className='flex w-full justify-between items-center'>
-        <div className='flex flex-col lg:w-7/12'>
+    <div className='container__inner hero__padding flex flex-col gap-6'>
+      <div className='flex flex-col lg:flex-row gap-8 w-full justify-between items-center'>
+        <div className='flex flex-col lg:w-6/12 lg:pr-4'>
           <div className='hero__headline'>
-            {slice.primary.headline ? (
-              <PrismicRichText field={slice.primary.headline} />
-            ) : (
-              <h2>Template slice, update me!</h2>
-            )}
+            <PrismicRichText field={slice.primary.headline} />
           </div>
           <div className='text-gray-400 text-lg xl:text-xl leading-relaxed mt-8 lg:mt-12 max-w-prose'>
-            {slice.primary.subheadline ? (
-              <PrismicRichText field={slice.primary.subheadline} />
-            ) : (
-              <p>start by editing this slice from inside Slice Machine!</p>
-            )}
+            <PrismicRichText field={slice.primary.subheadline} />
           </div>
         </div>
-        <div className='hidden lg:flex lg:justify-end lg:w-4/12'>
-          <img
-            src={slice.primary.hero_image.url}
-            alt={slice.primary.hero_image.alt}
-            className='w-4/5'
-          />
+        <div className='flex lg:justify-end w-full lg:w-6/12 xl:w-5/12'>
+          <YouTubeEmbed embedid={slice.primary.youtube_id} />
         </div>
       </div>
-      <div className='bg-base-dark flex flex-col lg:flex-row mt-12 lg:mt-16 pt-5 pb-6 px-4 lg:px-8 gap-6 rounded'>
-        <div className='text-white font-semibold text-2xl lg:text-3xl leading-tight w-full lg:w-5/12 flex justify-center items-center'>
-          {slice.primary.cta_headline}
-        </div>
-        <div className='bg-base-light rounded flex flex-col lg:flex-row px-4 py-4 gap-3 w-full lg:w-7/12'>
-          <div className='flex w-full '>
-            <div className='flex flex-col  w-1/4 justify-center items-center'>
-              <div className='text-base-dark font-plex font-bold text-3xl'>
-                06
-              </div>
-              <div className='text-base-dark font-plex uppercase'>Days</div>
-            </div>
-            <div className='flex flex-col w-1/4 justify-center items-center'>
-              <div className='text-base-dark font-plex font-bold text-3xl'>
-                12
-              </div>
-              <div className='text-base-dark font-plex uppercase'>Hours</div>
-            </div>
-            <div className='flex flex-col w-1/4 justify-center items-center'>
-              <div className='text-base-dark font-plex font-bold text-3xl'>
-                41
-              </div>
-              <div className='text-base-dark font-plex uppercase'>Min</div>
-            </div>
-            <div className='flex flex-col w-1/4 justify-center items-center'>
-              <div className='text-base-dark font-plex font-bold text-3xl'>
-                42
-              </div>
-              <div className='text-base-dark font-plex uppercase'>Sec</div>
-            </div>
-          </div>
-          <CTAButtonLarge />
-        </div>
-      </div>
+      <CTACountdown headline={slice.primary.cta_headline} />
     </div>
   </section>
 );
