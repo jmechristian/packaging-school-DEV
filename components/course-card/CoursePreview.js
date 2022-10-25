f4759b28268d64a6d16d0578ab8c9c804ef86270
jmechristian <jmechristian@gmail.com>
2022-10-24 22:16:08 -0400

    z-index

--- components/course-card/CoursePreview.js
+++ components/course-card/CoursePreview.js
@@ -5,15 +5,15 @@ import YouTubeEmbed from '../YouTubeEmbed';
 const CoursePreview = ({ close, video }) => {
   return (
     <>
-      <div className='fixed z-50 top-0 right-0'>
+      <div className='fixed z-[650] top-0 right-0'>
         <button onClick={() => close()}>
           <XCircleIcon className='w-8 h-8 fill-slate-300' />
         </button>
       </div>
-      <div className='fixed z-50 left-0 right-0 inset-y-1/2 -translate-y-1/2 aspect-video'>
+      <div className='fixed z-[60] left-0 right-0 inset-y-1/2 -translate-y-1/2 aspect-video'>
         <YouTubeEmbed embedid={video} />
       </div>
-      <div className='fixed top-0 right-0 bottom-0 left-0 bg-slate-900/90 z-40 backdrop-blur-sm'></div>
+      <div className='fixed top-0 right-0 bottom-0 left-0 bg-slate-900/90 z-[55] backdrop-blur-sm'></div>
     </>
   );
 };
