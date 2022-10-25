923cc698151acde5e6aa914208a844a610e4864b
jmechristian <jmechristian@gmail.com>
2022-10-24 22:11:07 -0400

    relative

--- components/course-card/CoursePreview.js
+++ components/course-card/CoursePreview.js
@@ -5,7 +5,7 @@ import YouTubeEmbed from '../YouTubeEmbed';
 const CoursePreview = ({ close, video }) => {
   return (
     <>
-      <div className='absolute z-[60] top-0 right-0'>
+      <div className='fixed z-50 top-0 right-0'>
         <button onClick={() => close()}>
           <XCircleIcon className='w-8 h-8 fill-slate-300' />
         </button>
