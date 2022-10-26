a68b37c1edbf543e5a6d2a69fb07b3c4979efab9
jmechristian <jmechristian@gmail.com>
2022-10-26 17:31:56 -0400

    drag done?

--- features/all_courses/AllCourseSlider.js
+++ features/all_courses/AllCourseSlider.js
@@ -3,29 +3,35 @@ import CourseCard from '../../components/course-card/CourseCard';
 import { motion } from 'framer-motion';
 
 const AllCourseSlider = ({ coursesToShow }) => {
-  const cardRef = useRef();
+  const cardRef = useRef(null);
   const [width, setWidth] = useState(0);
 
-  // useEffect(() => {
-  //   setWidth(cardRef.current.offsetWidth - cardRef.current.scrollWidth);
-  // }, []);
+  useEffect(() => {
+    setWidth(cardRef.current.offsetWidth - cardRef.current.scrollWidth);
+  }, []);
 
   return (
-    <div className='gap-6 grid grid-rows-2 grid-flow-col overflow-y-hidden'>
-      {coursesToShow.map((course, i) => (
-        <div key={i} className='h-full'>
-          <CourseCard
-            title={course.node.course_title[0].text}
-            desc={course.node.course_subtitle[0].text}
-            video={course.node.embed_id}
-            hours={course.node.course_hours}
-            lessons={course.node.course_lessons}
-            price={course.node.course_price}
-            slug={course.node._meta.uid}
-          />
-        </div>
-      ))}
-    </div>
+    <motion.div className='overflow-hidden' ref={cardRef}>
+      <motion.div
+        className='gap-6 grid grid-flow-col grid-rows-2 container__inner'
+        drag='x'
+        dragConstraints={{ right: 0, left: width, top: 0, bottom: 0 }}
+      >
+        {coursesToShow.map((course, i) => (
+          <motion.div key={i} className='h-full'>
+            <CourseCard
+              title={course.node.course_title[0].text}
+              desc={course.node.course_subtitle[0].text}
+              video={course.node.embed_id}
+              hours={course.node.course_hours}
+              lessons={course.node.course_lessons}
+              price={course.node.course_price}
+              slug={course.node._meta.uid}
+            />
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
   );
 };
 
