07e599423c66840a0827322e3531cd960abfd782
jmechristian <jmechristian@gmail.com>
2022-10-26 18:03:51 -0400

    sdsdsdsd

--- features/all_courses/AllCourseSlider.js
+++ features/all_courses/AllCourseSlider.js
@@ -6,25 +6,31 @@ const AllCourseSlider = ({ coursesToShow }) => {
   const cardRef = useRef(null);
   const [width, setWidth] = useState(0);
 
+  useEffect(() => {
+    setWidth(cardRef.current.offsetWidth - cardRef.current.scrollWidth);
+  }, []);
+
   return (
-    <div
-      className='gap-6 grid grid-flow-col grid-rows-2 container__inner overflow-auto touch-manipulation relative w-full'
-      // drag='x'
-      // dragConstraints={{ right: 0, left: width, top: 0, bottom: 0 }}
-    >
-      {coursesToShow.map((course, i) => (
-        <div key={i} className='h-full'>
-          <CourseCard
-            title={course.node.course_title[0].text}
-            desc={course.node.course_subtitle[0].text}
-            video={course.node.embed_id}
-            hours={course.node.course_hours}
-            lessons={course.node.course_lessons}
-            price={course.node.course_price}
-            slug={course.node._meta.uid}
-          />
-        </div>
-      ))}
+    <div className='touch-manipulation overflow-auto relative' ref={cardRef}>
+      <div
+        className='gap-6 grid grid-flow-col grid-rows-2 container__inner'
+        // drag='x'
+        // dragConstraints={{ right: 0, left: width, top: 0, bottom: 0 }}
+      >
+        {coursesToShow.map((course, i) => (
+          <div key={i} className='h-full'>
+            <CourseCard
+              title={course.node.course_title[0].text}
+              desc={course.node.course_subtitle[0].text}
+              video={course.node.embed_id}
+              hours={course.node.course_hours}
+              lessons={course.node.course_lessons}
+              price={course.node.course_price}
+              slug={course.node._meta.uid}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
