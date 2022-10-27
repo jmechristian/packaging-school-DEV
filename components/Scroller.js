d20e609b5ce06124a14950bc0cbec2319b7dd10e
jmechristian <jmechristian@gmail.com>
2022-10-27 14:39:34 -0400

    implic

--- components/Scroller.js
+++ components/Scroller.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Scroller = ({ children, rows }) => {
+const Scroller = ({ rows, children }) => {
   return (
     <div
-      className={`grid grid-flow-col grid-rows-${rows} auto-cols-max gap-4 overflow-auto overflow-y-hidden snap-x snap-mandatory snap-always overscroll-contain touch-manipulation px-6 scroll-p-6 md:px-24 md:scroll-p-24`}
+      className={`grid grid-flow-col grid-rows-2 auto-cols-max gap-4 overflow-auto overflow-y-hidden snap-x snap-mandatory snap-always touch-manipulation px-6 scroll-p-6 md:px-24 md:scroll-p-24`}
       id='scrollers'
     >
       {children}
