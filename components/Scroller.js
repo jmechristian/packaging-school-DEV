95f1290ff75add1f6c028ff41c87a96a8233bdda
jmechristian <jmechristian@gmail.com>
2022-10-27 14:31:16 -0400

    width?

--- components/Scroller.js
+++ components/Scroller.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Scroller = ({ children, rows }) => {
   return (
     <div
-      className={`relative grid grid-flow-col grid-rows-${rows} gap-4 auto-cols-max overflow-y-hidden overflow-auto snap-x snap-mandatory overscroll-contain  touch-manipulation px-6 scroll-p-6 md:px-24 md:scroll-p-24`}
+      className={`w-screen relative grid grid-flow-col grid-rows-${rows} gap-4  overflow-y-hidden overflow-auto snap-x snap-mandatory overscroll-contain  touch-manipulation px-6 scroll-p-6 md:px-24 md:scroll-p-24`}
       id='scrollers'
     >
       {children}
