89cf4d05e66cb65f2963791687ed48ebb99e3cab
jmechristian <jmechristian@gmail.com>
2022-10-26 20:13:57 -0400

    stack grid

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -9,9 +9,19 @@ const Sandbox = () => {
             <div className='pt-4'>
               <div className='relative'>
                 <div
-                  className='w-screen relative left-1/2 right-1/2 pl-9 scroll-p-10 grid grid-flow-col auto-cols-max gap-6 overflow-auto overflow-y-hidden snap-x snap-mandatory snap-always touch-manipulation'
+                  className='w-screen relative left-1/2 right-1/2 pl-9 scroll-p-10 grid grid-flow-col grid-rows-2 auto-cols-max gap-6 overflow-auto overflow-y-hidden snap-x snap-mandatory snap-always touch-manipulation'
                   style={{ marginLeft: '-50vw', right: '-50vw' }}
                 >
+                  <div>
+                    <div className='relative'>
+                      <div className='w-72 h-72 bg-red-500 snap-start'></div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='relative'>
+                      <div className='w-72 h-72 bg-red-500 snap-start'></div>
+                    </div>
+                  </div>
                   <div>
                     <div className='relative'>
                       <div className='w-72 h-72 bg-red-500 snap-start'></div>
