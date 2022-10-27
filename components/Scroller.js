2948298aaf833c503492af088532b8c5ebc2c8e2
jmechristian <jmechristian@gmail.com>
2022-10-27 14:25:19 -0400

    sdsd

--- components/Scroller.js
+++ components/Scroller.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import CourseCard from './course-card/CourseCard';
 
 const Scroller = ({ children, rows }) => {
   return (
     <div
-      className={`grid grid-flow-col gap-4 auto-cols-max overflow-y-hidden overflow-auto snap-x snap-mandatory overscroll-contain grid-rows-${rows} touch-manipulation px-6 scroll-p-6 md:px-24 md:scroll-p-24`}
+      className={`grid grid-flow-col grid-rows-${rows} gap-4 auto-cols-max overflow-y-hidden overflow-auto snap-x snap-mandatory overscroll-contain  touch-manipulation px-6 scroll-p-6 md:px-24 md:scroll-p-24`}
       id='scrollers'
     >
       {children}
