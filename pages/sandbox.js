a40092f4d4da88ea7e988ebf82eb9e10cc96f6fa
jmechristian <jmechristian@gmail.com>
2022-10-26 20:08:18 -0400

    sandbox

---
+++ pages/sandbox.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Sandbox = () => {
+  return (
+    <div className='w-full flex grow shrink-0 basis-auto m-0'>
+      <div className='w-full mx-auto pl-8 pr-8'>
+        <div className='block'>
+          <div>
+            <div className='pt-4'>
+              <div className='relative'>
+                <div
+                  className='w-screen relative left-1/2 right-1/2 pl-9 scroll-p-10 grid grid-flow-col auto-cols-max gap-6 overflow-auto overflow-y-hidden snap-x snap-mandatory snap-always touch-manipulation'
+                  style={{ marginLeft: '-50vw', right: '-50vw' }}
+                >
+                  <div>
+                    <div className='relative'>
+                      <div className='w-72 h-72 bg-red-500 snap-start'></div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='relative'>
+                      <div className='w-72 h-72 bg-red-500 snap-start'></div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='relative'>
+                      <div className='w-72 h-72 bg-red-500 snap-start'></div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='relative'>
+                      <div className='w-72 h-72 bg-red-500 snap-start'></div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='relative'>
+                      <div className='w-72 h-72 bg-red-500 snap-start'></div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='relative'>
+                      <div className='w-72 h-72 bg-red-500 snap-start'></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sandbox;
