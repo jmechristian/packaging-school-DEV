6cc593d61ec5354344e5953a260c01c68eab5f37
jmechristian <jmechristian@gmail.com>
2022-10-27 12:55:53 -0400

    overscroll

--- components/Scroller.js
+++ components/Scroller.js
@@ -4,7 +4,7 @@ import CourseCard from './course-card/CourseCard';
 const Scroller = ({ children, rows }) => {
   return (
     <div
-      className={`w-screen relative left-1/2 right-1/2 px-6 scroll-p-10 grid grid-flow-col grid-rows-${rows} auto-cols-max gap-4 overflow-auto overflow-y-hidden snap-x snap-mandatory snap-always touch-manipulation`}
+      className={`w-screen relative left-1/2 right-1/2 px-6 scroll-p-10 grid grid-flow-col grid-rows-${rows} auto-cols-max gap-4 overflow-auto overflow-y-hidden snap-x snap-mandatory snap-always touch-manipulation overscroll-contain`}
       style={{ marginLeft: '-50vw', right: '-50vw' }}
       id='scrollers'
     >
