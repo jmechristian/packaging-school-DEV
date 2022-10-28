90bbc16ed84cb82d738644585eb5f01ceb3ebba0
jmechristian <jmechristian@gmail.com>
2022-10-28 14:28:52 -0400

    ssdsd

--- features/all_courses/AllCourseBodyDesktop.js
+++ features/all_courses/AllCourseBodyDesktop.js
@@ -8,7 +8,7 @@ import AllCourseSearch from './AllCourseSearch';
 const AllCourseBodyDesktop = ({ coursesToShow }) => {
   const { selectedFilter } = useSelector((state) => state.course_filter);
   return (
-    <div className='w-full h-full relative hidden lg:flex gap-16 xl:gap-20 container__inner py-12'>
+    <div className='w-full h-full relative hidden lg:flex gap-16 xl:gap-20 container__inner py-16'>
       <div className='w-1/3 xl:w-1/4'>
         <div className='w-full sticky top-32'>
           <AllCourseMenu />
