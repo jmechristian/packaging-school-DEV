5f7a5ed7f091decce1acfa8526bc2d2121ee8baa
jmechristian <jmechristian@gmail.com>
2022-10-28 14:27:29 -0400

    sde

--- features/all_courses/AllCoursesCert.js
+++ features/all_courses/AllCoursesCert.js
@@ -4,8 +4,7 @@ const AllCoursesCert = () => {
   return (
     <div className=' flex flex-col gap-6'>
       <div className='font-medium text-xl leading-tight col-span-4'>
-        Comprehensive <span className='font-bold'>Certificate Programs</span>{' '}
-        Led By Subject Matter Experts
+        Comprehensive <span className='font-bold'>Certificate Programs</span>
       </div>
       <div className='relative flex flex-col gap-6 lg:grid lg:grid-cols-2 xl:grid-cols-3'>
         <div className='bg-base-brand w-full h-44 rounded-md'></div>
