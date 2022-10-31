1980643f73746ae362314a787107ac6abbefe251
jmechristian <jmechristian@gmail.com>
2022-10-31 17:12:53 -0400

    as

--- features/all_courses/AllCourseBodyDesktop.js
+++ features/all_courses/AllCourseBodyDesktop.js
@@ -8,14 +8,14 @@ import AllCourseSearch from './AllCourseSearch';
 const AllCourseBodyDesktop = ({ coursesToShow }) => {
   const { selectedFilter } = useSelector((state) => state.course_filter);
   return (
-    <div className='w-full h-full relative hidden lg:flex gap-16 xl:gap-20 container__inner py-16'>
+    <div className='w-full h-full relative hidden lg:flex gap-16 xl:gap-20 container__inner py-12'>
       <div className='w-1/3 xl:w-1/4'>
         <div className='w-full sticky top-32'>
           <AllCourseMenu />
         </div>
       </div>
       <div className='w-2/3 xl:w-3/4 flex flex-col gap-10'>
-        <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-6'>
           <div className='lg:text-4xl font-plex font-bold xl:leading-tight '>
             <h1>Accelerate your career. Stand out from your peers.</h1>
           </div>
