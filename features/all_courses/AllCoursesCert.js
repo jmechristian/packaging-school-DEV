c4bc5b9af65701b2aefabe8d45f912bf3009c98d
jmechristian <jmechristian@gmail.com>
2022-10-31 13:44:20 -0400

    sdd

--- features/all_courses/AllCoursesCert.js
+++ features/all_courses/AllCoursesCert.js
@@ -6,8 +6,21 @@ const AllCoursesCert = () => {
       <div className='font-medium text-xl leading-tight col-span-4'>
         Comprehensive <span className='font-bold'>Certificate Programs</span>
       </div>
-      <div className='relative flex flex-col gap-6 lg:grid lg:grid-cols-2 xl:grid-cols-3'>
-        <div className='bg-base-brand w-full h-44 rounded-md'></div>
+      <div className='relative flex flex-col gap-6 lg:grid lg:grid-cols-2'>
+        <div className='bg-dark-lg bg-cover w-full rounded-md shadow-md'>
+          <div className='flex flex-col w-full justify-start p-4 gap-6'>
+            <div className='font-plex font-bold text-lg text-white'>
+              Certificate of Mastery in Packaging Management
+            </div>
+            <div className='bg-slate-500/40 p-4 rounded-md backdrop-blur-lg shadow-xl'>
+              <div className='text-sm text-slate-200'>
+                An online education program that provides proactive learning in
+                the space of packaging development, material procurement, and
+                organizational management.
+              </div>
+            </div>
+          </div>
+        </div>
         <div className='bg-base-mid rounded-md h-44'></div>
         <div className='bg-base-brand w-full h-44 rounded-md'></div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,8 @@ module.exports = {
           "url('https://res.cloudinary.com/dno7xxmmy/image/upload/v1666045015/pschool/gradient-bg4_rrmnwf.webp')",
         'bootcamp-mobile':
           "url('https://res.cloudinary.com/dno7xxmmy/image/upload/v1666706513/pschool/bootcamp_bg_im0gtc.png')",
+        'cmpm-allcourse':
+          "url('https://res.cloudinary.com/dno7xxmmy/image/upload/v1667230225/pschool/cmpm_jqbxp0.png')",
       },
     },
   },
