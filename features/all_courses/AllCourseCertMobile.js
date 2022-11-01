b94cbb59a7ef3baaca66979a52f65850002df2ce
jmechristian <jmechristian@gmail.com>
2022-11-01 12:00:47 -0400

    all course final

--- features/all_courses/AllCourseCertMobile.js
+++ features/all_courses/AllCourseCertMobile.js
@@ -6,7 +6,10 @@ const AllCourseCertMobile = () => {
   const certRef = useRef();
 
   return (
-    <div className='flex'>
+    <div className='flex flex-col gap-4'>
+      <div className='font-medium text-lg leading-tight col-span-4 container__inner'>
+        Comprehensive <span className='font-bold'>Certificate Programs</span>
+      </div>
       <Scroller scroll={certRef} rows='1'>
         <CertificateCard />
         <CertificateCard />
