ff86fd9389ef43748be8b608520b61c125185395
jmechristian <jmechristian@gmail.com>
2022-11-08 16:05:58 -0500

    footer

---
+++ features/navigation/Footer/FooterCerts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FooterCerts = () => {
+  return (
+    <div className='flex flex-col gap-1 text-sm'>
+      <div className='font-extrabold text-sm uppercase tracking-widest text-slate-200'>
+        Certifications
+      </div>
+      <p className='text-slate-500'>
+        Certificate of Mastery in Packaging Management
+      </p>
+      <p className='text-slate-500'>Certificate of Packaging Science</p>
+      <p className='text-slate-500'>Automotive Packaging Certificate</p>
+    </div>
+  );
+};
+
+export default FooterCerts;
--- features/navigation/Footer/FooterMenu.js
+++ features/navigation/Footer/FooterMenu.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import FooterCerts from './FooterCerts';
+import FooterCourses from './FooterCourses';
 
 const FooterMenu = () => {
-  return <div>FooterMenu</div>;
+  return (
+    <div className='grid grid-cols-1 gap-8'>
+      <FooterCerts />
+      <FooterCourses />
+    </div>
+  );
 };
 
 export default FooterMenu;
