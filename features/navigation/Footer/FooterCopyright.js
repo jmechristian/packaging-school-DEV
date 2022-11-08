b07eb6e7ee1aec08a6cc8f24ed73eff963959864
jmechristian <jmechristian@gmail.com>
2022-11-08 16:41:32 -0500

    mobile footer layed out

--- features/navigation/Footer/FooterCopyright.js
+++ features/navigation/Footer/FooterCopyright.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import Image from 'next/image';
 
 const FooterCopyright = () => {
-  return <div>FooterCopyright</div>;
+  return (
+    <div className='flex flex-col gap-8'>
+      <div className='grid grid-cols-12 gap-4 overflow-hidden'>
+        <div className='col-span-3'>
+          <Image
+            src='https://res.cloudinary.com/dno7xxmmy/image/upload/v1667941998/pschool/packagingschool_logo_square-orange_d7dbzg.svg'
+            alt='The Packaging School Logo'
+            height='1121'
+            width='1078'
+            layout='responsive'
+          />
+        </div>
+        <div className='flex flex-col gap-2 col-span-9'>
+          <div className='text-slate-300 text-xs'>
+            South Carolina Commission on Higher Education License #5400
+          </div>
+          <div className='text-slate-500 text-xs'>
+            Copyright © 2015-2022 The Packaging School, LLC. All Rights
+            Reserved.
+          </div>
+        </div>
+      </div>
+      <div className='flex gap-3 text-xs text-slate-500'>
+        <div>Privacy Policy</div>
+        <div>Terms of Service</div>
+      </div>
+    </div>
+  );
 };
 
 export default FooterCopyright;
--- features/navigation/Footer/FooterCourses.js
+++ features/navigation/Footer/FooterCourses.js
@@ -7,12 +7,17 @@ const FooterCourses = () => {
         Courses
       </div>
       <div className='grid grid-cols-2 gap-y-1 gap-x-8 justify-between'>
-        <p className='text-slate-500 w-fit'>Materials</p>
-        <p className='text-slate-500 w-fit'>Industry</p>
-        <p className='text-slate-500 w-fit'>Food & Beverage</p>
-        <p className='text-slate-500 w-fit'>Business</p>
-        <p className='text-slate-500 w-fit'>Supply Chain & Logistics</p>
-        <p className='text-slate-500 w-fit'>Design</p>
+        <div className='flex flex-col gap-1'>
+          <p className='text-slate-500 w-fit'>All Courses</p>
+          <p className='text-slate-500 w-fit'>Materials</p>
+          <p className='text-slate-500 w-fit'>Industry</p>
+          <p className='text-slate-500 w-fit'>Food & Beverage</p>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <p className='text-slate-500 w-fit'>Business</p>
+          <p className='text-slate-500 w-fit'>Supply Chain & Logistics</p>
+          <p className='text-slate-500 w-fit'>Design</p>
+        </div>
       </div>
     </div>
   );
