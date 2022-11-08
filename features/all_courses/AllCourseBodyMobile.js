751869f35143f45be29010817e77612929669cff
jmechristian <jmechristian@gmail.com>
2022-11-08 10:07:06 -0500

    mobile

--- features/all_courses/AllCourseBodyMobile.js
+++ features/all_courses/AllCourseBodyMobile.js
@@ -7,9 +7,6 @@ const AllCourseBodyMobile = ({ coursesToShow }) => {
   return (
     <div className='flex flex-col gap-10 py-8 relative lg:hidden'>
       <div className='flex flex-col gap-4 container__inner'>
-        <div className='text-2xl md:text-3xl font-bold font-plex mb-4'>
-          <h2>Accelerate your career. Stand out amongst your peers.</h2>
-        </div>
         <AllCourseSearch />
       </div>
       <AllCourseCertMobile />
--- features/all_courses/AllCourseHeaderMobile.js
+++ features/all_courses/AllCourseHeaderMobile.js
@@ -5,8 +5,9 @@ const AllCourseHeaderMobile = () => {
   return (
     <div className='flex justify-center item-center w-full bg-bootcamp-mobile bg-cover bg-center lg:hidden'>
       <div className='flex flex-col gap-4 justify-center items-center text-center py-8 px-8'>
-        <div className='text-white font-plex font-bold text-3xl'>
-          New to Packaging? Start Here!
+        <div className='text-white font-plex font-bold text-2xl'>
+          New to Packaging?
+          <br /> Start Here!
         </div>
         <div className='text-white'>
           Jumpstart your knowledge with a solid foundation in packaging basics.
