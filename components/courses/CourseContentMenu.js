2cc420bdad4d6ceadcf4c0cb52fd8c8820fdad00
jmechristian <jmechristian@gmail.com>
2022-11-15 15:22:04 -0500

    course content menu

--- components/courses/CourseContentMenu.js
+++ components/courses/CourseContentMenu.js
@@ -44,18 +44,20 @@ const CourseContentMenu = () => {
 
   return (
     <motion.div
-      className='fixed z-50 left-0 bottom-5 py-3 bg-slate-200/40 backdrop-blur-sm rounded-r-lg shadow-lg'
+      className='fixed z-50 left-0 bottom-5 py-2 md:py-3 bg-slate-200/40 backdrop-blur-sm rounded-r-lg shadow-lg'
       ref={wrapperRef}
       variants={variants}
       initial='hidden'
       animate={showBuyButtons ? 'show' : 'hidden'}
     >
-      <div className='flex px-5 gap-3'>
-        <button className='bg-clemson py-1.5 rounded-md shadow'>
-          <div className='text-sm font-semibold text-white px-3'>Buy Now</div>
+      <div className='flex px-4 md:px-5 gap-3 md:5'>
+        <button className='bg-clemson py-1.5 lg:py-2 rounded-md shadow'>
+          <div className='text-sm md:text-lg font-semibold text-white px-3 w-24 md:w-40'>
+            Enroll Now
+          </div>
         </button>
         <button className='bg-white border border-clemson py-1.5 rounded-md shadow'>
-          <div className='text-sm font-semibold text-clemson px-3'>
+          <div className='text-sm md:text-lg font-semibold text-clemson px-3 w-24 md:w-40'>
             Free Trial
           </div>
         </button>
