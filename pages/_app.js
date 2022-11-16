11f5dbf3a5279705339986c877264007d11e6d8e
jmechristian <jmechristian@gmail.com>
2022-11-16 08:59:27 -0500

    rebuild pre search

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css';
 import Layout from '../features/layout/Layout';
 import Link from 'next/link';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch } from 'react-instantsearch-hooks-web';
 import { PrismicProvider } from '@prismicio/react';
 import { PrismicPreview } from '@prismicio/next';
 import { linkResolver, repositoryName } from '../prismicio';
@@ -8,6 +10,11 @@ import { store } from '../features/store';
 import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }) {
+  const searchClient = algoliasearch(
+    'W0TY58JH2W',
+    '0014bb643f8b92e979f2e8c81e961ca6'
+  );
+
   return (
     <PrismicProvider
       linkResolver={linkResolver}
