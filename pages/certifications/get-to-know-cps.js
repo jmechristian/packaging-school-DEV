dfa599ac702f5f8195e7d78363536ce441912719
jmechristian <jmechristian@gmail.com>
2023-03-09 09:26:21 -0500

    qqq

--- pages/certifications/get-to-know-cps.js
+++ pages/certifications/get-to-know-cps.js
@@ -12,6 +12,7 @@ import {
   CalendarDaysIcon,
   UsersIcon,
 } from '@heroicons/react/24/outline';
+import { useSelector } from 'react-redux';
 
 const primaryFeatures = [
   {
@@ -36,44 +37,7 @@ const primaryFeatures = [
     icon: CalendarDaysIcon,
   },
 ];
-const secondaryFeatures = [
-  {
-    name: 'Push to deploy.',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates.',
-    description:
-      'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Simple queues.',
-    description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
-    icon: ArrowPathIcon,
-  },
-  {
-    name: 'Advanced security.',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
-    icon: FingerPrintIcon,
-  },
-  {
-    name: 'Powerful API.',
-    description:
-      'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: Cog6ToothIcon,
-  },
-  {
-    name: 'Database backups.',
-    description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ',
-    icon: ServerIcon,
-  },
-];
+
 const stats = [
   { id: 1, name: 'Course Hours', value: '60' },
   { id: 2, name: 'Courses Included', value: '12' },
@@ -170,65 +134,105 @@ const footerNavigation = {
   ],
 };
 
-export default function Example() {
+export const Page = () => {
+  const { darkMode } = useSelector((state) => state.layout);
+
   return (
-    <div className='slate-100 dark:bg-slate-900'>
+    <div className='bg-white dark:bg-slate-900'>
       <main>
         {/* Hero section */}
         <div className='relative isolate overflow-hidden'>
-          <svg
-            className='absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
-            aria-hidden='true'
-          >
-            <defs>
-              <pattern
-                id='983e3e4c-de6d-4c3f-8d64-b9761d1534cc'
-                width={200}
-                height={200}
-                x='50%'
-                y={-1}
-                patternUnits='userSpaceOnUse'
+          {darkMode ? (
+            <>
+              <svg
+                className='absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
+                aria-hidden='true'
               >
-                <path d='M.5 200V.5H200' fill='none' />
-              </pattern>
-            </defs>
-            <svg x='50%' y={-1} className='overflow-visible fill-slate-800/20'>
-              <path
-                d='M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z'
+                <defs>
+                  <pattern
+                    id='983e3e4c-de6d-4c3f-8d64-b9761d1534cc'
+                    width={200}
+                    height={200}
+                    x='50%'
+                    y={-1}
+                    patternUnits='userSpaceOnUse'
+                  >
+                    <path d='M.5 200V.5H200' fill='none' />
+                  </pattern>
+                </defs>
+                <svg
+                  x='50%'
+                  y={-1}
+                  className='overflow-visible fill-slate-50 dark:fill-slate-800/20'
+                >
+                  <path
+                    d='M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z'
+                    strokeWidth={0}
+                  />
+                </svg>
+                <rect
+                  width='100%'
+                  height='100%'
+                  strokeWidth={0}
+                  fill='url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)'
+                />
+              </svg>
+              <svg
+                viewBox='0 0 1108 632'
+                aria-hidden='true'
+                className='absolute top-10 left-[calc(50%-4rem)] -z-10 w-[69.25rem] max-w-none transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]'
+              >
+                <path
+                  fill='url(#175c433f-44f6-4d59-93f0-c5c51ad5566d)'
+                  fillOpacity='.2'
+                  d='M235.233 402.609 57.541 321.573.83 631.05l234.404-228.441 320.018 145.945c-65.036-115.261-134.286-322.756 109.01-230.655C968.382 433.026 1031 651.247 1092.23 459.36c48.98-153.51-34.51-321.107-82.37-385.717L810.952 324.222 648.261.088 235.233 402.609Z'
+                />
+                <defs>
+                  <linearGradient
+                    id='175c433f-44f6-4d59-93f0-c5c51ad5566d'
+                    x1='1220.59'
+                    x2='-85.053'
+                    y1='432.766'
+                    y2='638.714'
+                    gradientUnits='userSpaceOnUse'
+                  >
+                    <stop stopColor='#04607e' />
+                    <stop offset={1} stopColor='#04607e' />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </>
+          ) : (
+            <svg
+              className='absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
+              aria-hidden='true'
+            >
+              <defs>
+                <pattern
+                  id='83fd4e5a-9d52-42fc-97b6-718e5d7ee527'
+                  width={200}
+                  height={200}
+                  x='50%'
+                  y={-1}
+                  patternUnits='userSpaceOnUse'
+                >
+                  <path d='M100 200V.5M.5 .5H200' fill='none' />
+                </pattern>
+              </defs>
+              <svg x='50%' y={-1} className='overflow-visible fill-gray-50'>
+                <path
+                  d='M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z'
+                  strokeWidth={0}
+                />
+              </svg>
+              <rect
+                width='100%'
+                height='100%'
                 strokeWidth={0}
+                fill='url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)'
               />
             </svg>
-            <rect
-              width='100%'
-              height='100%'
-              strokeWidth={0}
-              fill='url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)'
-            />
-          </svg>
-          <svg
-            viewBox='0 0 1108 632'
-            aria-hidden='true'
-            className='absolute top-10 left-[calc(50%-4rem)] -z-10 w-[69.25rem] max-w-none transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]'
-          >
-            <path
-              fill='url(#175c433f-44f6-4d59-93f0-c5c51ad5566d)'
-              fillOpacity='.2'
-              d='M235.233 402.609 57.541 321.573.83 631.05l234.404-228.441 320.018 145.945c-65.036-115.261-134.286-322.756 109.01-230.655C968.382 433.026 1031 651.247 1092.23 459.36c48.98-153.51-34.51-321.107-82.37-385.717L810.952 324.222 648.261.088 235.233 402.609Z'
-            />
-            <defs>
-              <linearGradient
-                id='175c433f-44f6-4d59-93f0-c5c51ad5566d'
-                x1='1220.59'
-                x2='-85.053'
-                y1='432.766'
-                y2='638.714'
-                gradientUnits='userSpaceOnUse'
-              >
-                <stop stopColor='#04607e' />
-                <stop offset={1} stopColor='#04607e' />
-              </linearGradient>
-            </defs>
-          </svg>
+          )}
           <div className='mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-40 lg:flex lg:px-8 lg:pt-40'>
             <div className='mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8'>
               <div className='mt-24 sm:mt-32 lg:mt-16'>
@@ -427,61 +431,130 @@ export default function Example() {
 
         {/* CTA section */}
         <div className='relative isolate mt-32 px-6 py-32 sm:mt-56 sm:py-40 lg:px-8'>
-          <svg
-            className='absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
-            aria-hidden='true'
-          >
-            <defs>
-              <pattern
-                id='1d4240dd-898f-445f-932d-e2872fd12de3'
-                width={200}
-                height={200}
-                x='50%'
-                y={0}
-                patternUnits='userSpaceOnUse'
+          {darkMode ? (
+            <>
+              <svg
+                className='absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
+                aria-hidden='true'
               >
-                <path d='M.5 200V.5H200' fill='none' />
-              </pattern>
-            </defs>
-            <svg x='50%' y={0} className='overflow-visible fill-gray-800/20'>
-              <path
-                d='M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z'
-                strokeWidth={0}
-              />
-            </svg>
-            <rect
-              width='100%'
-              height='100%'
-              strokeWidth={0}
-              fill='url(#1d4240dd-898f-445f-932d-e2872fd12de3)'
-            />
-          </svg>
-          <div className='absolute inset-x-0 top-10 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl'>
-            <svg
-              viewBox='0 0 1108 632'
-              aria-hidden='true'
-              className='w-[69.25rem] flex-none'
-            >
-              <path
-                fill='url(#175c433f-44f6-4d59-93f0-c5c51ad5566d)'
-                fillOpacity='.2'
-                d='M235.233 402.609 57.541 321.573.83 631.05l234.404-228.441 320.018 145.945c-65.036-115.261-134.286-322.756 109.01-230.655C968.382 433.026 1031 651.247 1092.23 459.36c48.98-153.51-34.51-321.107-82.37-385.717L810.952 324.222 648.261.088 235.233 402.609Z'
-              />
-              <defs>
-                <linearGradient
-                  id='175c433f-44f6-4d59-93f0-c5c51ad5566d'
-                  x1='1220.59'
-                  x2='-85.053'
-                  y1='432.766'
-                  y2='638.714'
-                  gradientUnits='userSpaceOnUse'
+                <defs>
+                  <pattern
+                    id='1d4240dd-898f-445f-932d-e2872fd12de3'
+                    width={200}
+                    height={200}
+                    x='50%'
+                    y={0}
+                    patternUnits='userSpaceOnUse'
+                  >
+                    <path d='M.5 200V.5H200' fill='none' />
+                  </pattern>
+                </defs>
+                <svg
+                  x='50%'
+                  y={0}
+                  className='overflow-visible fill-gray-800/20'
                 >
-                  <stop stopColor='#4F46E5' />
-                  <stop offset={1} stopColor='#80CAFF' />
-                </linearGradient>
-              </defs>
-            </svg>
-          </div>
+                  <path
+                    d='M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z'
+                    strokeWidth={0}
+                  />
+                </svg>
+                <rect
+                  width='100%'
+                  height='100%'
+                  strokeWidth={0}
+                  fill='url(#1d4240dd-898f-445f-932d-e2872fd12de3)'
+                />
+              </svg>
+              <div className='absolute inset-x-0 top-10 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl'>
+                <svg
+                  viewBox='0 0 1108 632'
+                  aria-hidden='true'
+                  className='w-[69.25rem] flex-none'
+                >
+                  <path
+                    fill='url(#175c433f-44f6-4d59-93f0-c5c51ad5566d)'
+                    fillOpacity='.2'
+                    d='M235.233 402.609 57.541 321.573.83 631.05l234.404-228.441 320.018 145.945c-65.036-115.261-134.286-322.756 109.01-230.655C968.382 433.026 1031 651.247 1092.23 459.36c48.98-153.51-34.51-321.107-82.37-385.717L810.952 324.222 648.261.088 235.233 402.609Z'
+                  />
+                  <defs>
+                    <linearGradient
+                      id='175c433f-44f6-4d59-93f0-c5c51ad5566d'
+                      x1='1220.59'
+                      x2='-85.053'
+                      y1='432.766'
+                      y2='638.714'
+                      gradientUnits='userSpaceOnUse'
+                    >
+                      <stop stopColor='#4F46E5' />
+                      <stop offset={1} stopColor='#80CAFF' />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+            </>
+          ) : (
+            <>
+              <svg
+                className='absolute inset-0 -z-10 hidden h-full w-full stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)] sm:block'
+                aria-hidden='true'
+              >
+                <defs>
+                  <pattern
+                    id='55d3d46d-692e-45f2-becd-d8bdc9344f45'
+                    width={200}
+                    height={200}
+                    x='50%'
+                    y={0}
+                    patternUnits='userSpaceOnUse'
+                  >
+                    <path d='M.5 200V.5H200' fill='none' />
+                  </pattern>
+                </defs>
+                <svg x='50%' y={0} className='overflow-visible fill-gray-50'>
+                  <path
+                    d='M-200.5 0h201v201h-201Z M599.5 0h201v201h-201Z M399.5 400h201v201h-201Z M-400.5 600h201v201h-201Z'
+                    strokeWidth={0}
+                  />
+                </svg>
+                <rect
+                  width='100%'
+                  height='100%'
+                  strokeWidth={0}
+                  fill='url(#55d3d46d-692e-45f2-becd-d8bdc9344f45)'
+                />
+              </svg>
+              <div className='relative'>
+                <div className='absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl'>
+                  <svg
+                    viewBox='0 0 1313 771'
+                    aria-hidden='true'
+                    className='ml-[max(50%,38rem)] w-[82.0625rem]'
+                  >
+                    <path
+                      id='bc169a03-3518-42d4-ab1e-d3eadac65edc'
+                      fill='url(#85a0eca5-25f1-4ab9-af84-4e2d8d9cdbf3)'
+                      d='M360.508 589.796 231.671 770.522 0 498.159l360.508 91.637 232.034-325.485c1.485 150.396 51.235 393.965 238.354 165.069C1064.79 143.261 1002.42-107.094 1171.72 46.97c135.44 123.252 148.51 335.641 138.11 426.428L971.677 339.803l24.062 411.461-635.231-161.468Z'
+                    />
+                    <defs>
+                      <linearGradient
+                        id='85a0eca5-25f1-4ab9-af84-4e2d8d9cdbf3'
+                        x1='1313.17'
+                        x2='-88.881'
+                        y1='.201'
+                        y2='539.417'
+                        gradientUnits='userSpaceOnUse'
+                      >
+                        <stop stopColor='#9089FC' />
+                        <stop offset={1} stopColor='#FF80B5' />
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                </div>
+              </div>
+            </>
+          )}
+
           <div className='mx-auto max-w-2xl text-center'>
             <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>
               Boost your productivity.
@@ -511,4 +584,6 @@ export default function Example() {
       </main>
     </div>
   );
-}
+};
+
+export default Page;
