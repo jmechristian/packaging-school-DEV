d597a4ec0028dde4a9d2da6e11a7012535598799
jmechristian <jmechristian@gmail.com>
2023-03-13 12:23:18 -0400

    sddd

--- pages/certifications/get-to-know-cps.js
+++ pages/certifications/get-to-know-cps.js
@@ -115,6 +115,7 @@ export async function getStaticProps() {
                 title
                 video
                 videos
+                subhead
               }
             }
           }
