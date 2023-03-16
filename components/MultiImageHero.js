70481302fafd177be6f6b0c430f726f9a924ca2d
jmechristian <jmechristian@gmail.com>
2023-03-16 14:13:10 -0400

    sdssdsd

--- components/MultiImageHero.js
+++ components/MultiImageHero.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import HeadlineMotion from '../helpers/HeadlineMotion';
+import { motion, useInView, AnimatePresence } from 'framer-motion';
 
 const MultiImageHero = () => {
+  const imagesRef = useRef();
+  const isInView = useInView(imagesRef);
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.25,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  };
+
   return (
     <div className='relative isolate -z-10'>
       <svg
@@ -63,16 +84,29 @@ const MultiImageHero = () => {
           <div className='mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center'>
             <div className='w-full max-w-xl lg:shrink-0 xl:max-w-2xl'>
               <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl font-greycliff'>
-                Are You Ready To Work Together?
+                <HeadlineMotion>Are You Ready To Work Together?</HeadlineMotion>
               </h1>
-              <p className='relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none'>
+              <motion.p
+                className='relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none'
+                animate={isInView && { opacity: 1, transition: { delay: 0.4 } }}
+                initial={{ opacity: 0 }}
+              >
                 Empower Your Workforce: Collaborate with a Versatile Online
                 Education Partner for Tailored Learning Solutions, Professional
                 Development, and Hands-On Training Opportunities.
-              </p>
+              </motion.p>
             </div>
-            <div className='mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0'>
-              <div className='ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80'>
+            <motion.div
+              className='mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0'
+              ref={imagesRef}
+              variants={container}
+              initial='hidden'
+              animate='show'
+            >
+              <motion.div
+                className='ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80'
+                variants={item}
+              >
                 <div className='relative'>
                   <img
                     src='https://packschool.s3.amazonaws.com/partner-1.webp'
@@ -81,8 +115,11 @@ const MultiImageHero = () => {
                   />
                   <div className='pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10' />
                 </div>
-              </div>
-              <div className='mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36'>
+              </motion.div>
+              <motion.div
+                className='mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36'
+                variants={item}
+              >
                 <div className='relative'>
                   <img
                     src='https://packschool.s3.amazonaws.com/partner-2.webp'
@@ -99,8 +136,11 @@ const MultiImageHero = () => {
                   />
                   <div className='pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10' />
                 </div>
-              </div>
-              <div className='w-44 flex-none space-y-8 pt-32 sm:pt-0'>
+              </motion.div>
+              <motion.div
+                className='w-44 flex-none space-y-8 pt-32 sm:pt-0'
+                variants={item}
+              >
                 <div className='relative'>
                   <img
                     src='https://packschool.s3.amazonaws.com/partner-4.webp'
@@ -117,8 +157,8 @@ const MultiImageHero = () => {
                   />
                   <div className='pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10' />
                 </div>
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
           </div>
         </div>
       </div>
---
+++ helpers/SplitText.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+export function SplitText({ children, ...rest }) {
+  let words = children.split(' ');
+  return words.map((word, i) => {
+    return (
+      <div
+        key={children + i}
+        style={{
+          display: 'inline-block',
+          overflow: 'hidden',
+          paddingBottom: '6px',
+          lineHeight: 1,
+        }}
+      >
+        <motion.div
+          {...rest}
+          style={{ display: 'inline-block', willChange: 'transform' }}
+          custom={i}
+        >
+          {word + (i !== words.length - 1 ? '\u00A0' : '')}
+        </motion.div>
+      </div>
+    );
+  });
+}
