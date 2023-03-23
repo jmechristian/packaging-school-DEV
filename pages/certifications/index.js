779c9e1260ebba5d390cb3ae49141698b68e2d46
jmechristian <jmechristian@gmail.com>
2023-03-23 16:57:23 -0400

    ddfdf

--- pages/certifications/index.js
+++ pages/certifications/index.js
@@ -33,7 +33,7 @@ const Index = () => {
             </div>
             <img
               src='https://packschool.s3.amazonaws.com/chris-marsh-psgrad02.jpeg'
-              alt=''
+              alt='Certificate of Packaging Science Graduate'
               className='mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36'
             />
           </div>
