ae7c19249084bc727f17fdf395dd10f5c58eaecd
jmechristian <jmechristian@gmail.com>
2023-03-23 14:31:32 -0400

    ssss

---
+++ components/certifications/ChooseCert.js
@@ -0,0 +1,171 @@
+import { useState } from 'react';
+import { RadioGroup } from '@headlessui/react';
+import { CheckIcon } from '@heroicons/react/20/solid';
+
+const frequencies = [
+  { value: 'monthly', label: 'Monthly', priceSuffix: '/month' },
+  { value: 'full', label: 'Full', priceSuffix: 'in full' },
+];
+const tiers = [
+  {
+    name: 'Certificate of Packaging Science',
+    id: 'tier-freelancer',
+    href: '/certificates/get-to-know-cps',
+    price: { monthly: '$349', full: '$3,999' },
+    description:
+      'The Certificate of Packaging Science is a one year online program that teaches the materials, processes, and influences shaping the advancement of the industry.',
+    features: [
+      '12-months access',
+      'Choose your own electives',
+      '12 expert-led courses',
+      'Instructor email access',
+    ],
+    mostPopular: false,
+    bottom:
+      'Application Required. Summer Session 2023: May 22 - Aug 11. Application deadline: May 5th',
+  },
+  {
+    name: 'Certificate of Mastery in Packaging Management',
+    id: 'tier-startup',
+    href: '/certificates/get-to-know-cmpm',
+    price: { monthly: '$xx', full: '$7,000' },
+    description:
+      'The Certificate of Mastery in Packaging Management is an online executive education program that can level up your career in the space of packaging development, material procurement, and organizational management.',
+    features: [
+      '12-week structure',
+      'Flexible lectures and 1:1 instructor coaching',
+      'PhD Instructor-led project',
+      'Practical knowledge and rigorous examination',
+      'No prerequisites',
+    ],
+    mostPopular: true,
+  },
+  {
+    name: 'Automotive Packaging Certificate',
+    id: 'tier-enterprise',
+    href: '/certificates/get-to-know-aps',
+    price: { monthly: '$287', full: '$2,400' },
+    description:
+      'The first and only 100% online academic program that will enable you to develop the professional skill set you need to be successful in the automotive packaging field.',
+    features: [
+      'Returnable/ expendable packaging',
+      'Packaging distributors',
+      'Packaging ops for OEMs/ Tier 1s',
+      'Transportation',
+      'Hazmat packaging',
+    ],
+    mostPopular: false,
+  },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function ChooseCert() {
+  const [frequency, setFrequency] = useState(frequencies[1]);
+
+  return (
+    <div className='bg-white py-24 sm:py-32'>
+      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+        <div className='mx-auto max-w-4xl text-center'>
+          <p className='mt-2 text-4xl font-bold tracking-tight font-greycliff text-gray-900 sm:text-5xl'>
+            The Right Subject Matter and Structure to Boost Your Knowledge.
+          </p>
+        </div>
+        <p className='mx-auto mt-6 max-w-3xl text-center text-lg leading-8 text-slate-600'>
+          Whatever the case, as the exclusive licensee of a professional
+          packaging curriculum developed at Clemson University, The Packaging
+          School will help raise your packaging IQ.
+        </p>
+        <div className='mt-16 flex justify-center'>
+          <RadioGroup
+            value={frequency}
+            onChange={setFrequency}
+            className='grid grid-cols-2 gap-x-1 rounded-full p-1 text-center text-xs font-semibold leading-5 ring-1 ring-inset ring-slate-200'
+          >
+            <RadioGroup.Label className='sr-only'>
+              Payment frequency
+            </RadioGroup.Label>
+            {frequencies.map((option) => (
+              <RadioGroup.Option
+                key={option.value}
+                value={option}
+                className={({ checked }) =>
+                  classNames(
+                    checked ? 'bg-base-mid text-white' : 'text-slate-500',
+                    'cursor-pointer rounded-full py-1 px-2.5'
+                  )
+                }
+              >
+                <span>{option.label}</span>
+              </RadioGroup.Option>
+            ))}
+          </RadioGroup>
+        </div>
+        <div className='isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
+          {tiers.map((tier) => (
+            <div
+              key={tier.id}
+              className={classNames(
+                tier.mostPopular
+                  ? 'ring-2 ring-clemson'
+                  : 'ring-1 ring-slate-200',
+                'rounded-3xl p-8 xl:p-10'
+              )}
+            >
+              <div className='flex items-center justify-between gap-x-4'>
+                <h3
+                  id={tier.id}
+                  className={classNames(
+                    tier.mostPopular ? 'text-base-mid' : 'text-slate-900',
+                    'text-xl md:text-2xl font-bold leading-8 font-greycliff'
+                  )}
+                >
+                  {tier.name}
+                </h3>
+              </div>
+              <p className='mt-4 leading-6 text-slate-600'>
+                {tier.description}
+              </p>
+              <p className='mt-6 flex items-baseline gap-x-1'>
+                <span className='text-4xl font-bold tracking-tight text-slate-900'>
+                  {tier.price[frequency.value]}
+                </span>
+                <span className='font-semibold leading-6 text-slate-600'>
+                  {frequency.priceSuffix}
+                </span>
+              </p>
+              <a
+                href={tier.href}
+                aria-describedby={tier.id}
+                className={classNames(
+                  tier.mostPopular
+                    ? 'bg-base-mid text-white shadow-sm hover:bg-base-brand'
+                    : 'text-base-mid ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300',
+                  'mt-6 block rounded-md py-2 px-3 text-center font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                )}
+              >
+                Enroll Now
+              </a>
+              <ul
+                role='list'
+                className='mt-8 space-y-3 leading-6 text-slate-600 xl:mt-10'
+              >
+                {tier.features.map((feature) => (
+                  <li key={feature} className='flex gap-x-3'>
+                    <CheckIcon
+                      className='h-6 w-5 flex-none text-base-mid'
+                      aria-hidden='true'
+                    />
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/certifications/index.js
+++ pages/certifications/index.js
@@ -1,7 +1,123 @@
 import React from 'react';
+import GradientCTA from '../../components/GradientCTA';
+import ChooseCert from '../../components/certifications/ChooseCert';
+import PatternBackground from '../../components/layout/PatternBackground';
+import { ArrowDownCircleIcon } from '@heroicons/react/24/solid';
 
-const index = () => {
-  return <div>index</div>;
+const Index = () => {
+  return (
+    <div className='scroll-smooth'>
+      <PatternBackground />
+      {/* Hero section */}
+      <div className='pt-14'>
+        <div className='mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8'>
+          <div className='mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8'>
+            <h1 className='max-w-2xl text-4xl font-bold tracking-tight font-greycliff text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto'>
+              Level Up with One of Our Elite Packaging Certificates
+            </h1>
+            <div className='mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1'>
+              <p className='text-lg md:text-xl text-gray-500 md:leading-9'>
+                Designed for those students who want to separate themselves from
+                their peers, these comprehensive certificate programs allow for
+                structured or self-paced instructor from the industry&apos;s
+                most trusted subject-matter experts.
+              </p>
+              <div className='flex gap-1 items-center mt-6 md:mt-12'>
+                <p className='font-greycliff font-bold text-lg md:text-2xl'>
+                  <a href='#perfectcert'>Find Your Perfect Certificate</a>
+                </p>
+                <div>
+                  <ArrowDownCircleIcon className='w-7 h-7 text-clemson' />
+                </div>
+              </div>
+            </div>
+            <img
+              src='https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80'
+              alt=''
+              className='mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36'
+            />
+          </div>
+        </div>
+        <div className='absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32' />
+      </div>
+      <div className='scroll-mt-16' id='perfectcert'>
+        <ChooseCert />
+      </div>
+      <section className='bg-white py-24 sm:py-32'>
+        <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+          <div className='mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
+            <div className='flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20'>
+              <img
+                className='h-12 self-start'
+                src='https://tailwindui.com/img/logos/tuple-logo-gray-900.svg'
+                alt=''
+              />
+              <figure className='mt-10 flex flex-auto flex-col justify-between'>
+                <blockquote className='text-lg leading-8 text-gray-900'>
+                  <p>
+                    “Amet amet eget scelerisque tellus sit neque faucibus non
+                    eleifend. Integer eu praesent at a. Ornare arcu gravida
+                    natoque erat et cursus tortor consequat at. Vulputate
+                    gravida sociis enim nullam ultricies habitant malesuada
+                    lorem ac. Tincidunt urna dui pellentesque sagittis.”
+                  </p>
+                </blockquote>
+                <figcaption className='mt-10 flex items-center gap-x-6'>
+                  <img
+                    className='h-14 w-14 rounded-full bg-gray-50'
+                    src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                    alt=''
+                  />
+                  <div className='text-base'>
+                    <div className='font-semibold text-gray-900'>
+                      Judith Black
+                    </div>
+                    <div className='mt-1 text-gray-500'>CEO of Tuple</div>
+                  </div>
+                </figcaption>
+              </figure>
+            </div>
+            <div className='flex flex-col border-t border-gray-900/10 pt-10 sm:pt-16 lg:border-t-0 lg:border-l lg:pt-0 lg:pl-8 xl:pl-20'>
+              <img
+                className='h-12 self-start'
+                src='https://tailwindui.com/img/logos/reform-logo-gray-900.svg'
+                alt=''
+              />
+              <figure className='mt-10 flex flex-auto flex-col justify-between'>
+                <blockquote className='text-lg leading-8 text-gray-900'>
+                  <p>
+                    “Excepteur veniam labore ullamco eiusmod. Pariatur consequat
+                    proident duis dolore nulla veniam reprehenderit nisi officia
+                    voluptate incididunt exercitation exercitation elit. Nostrud
+                    veniam sint dolor nisi ullamco.”
+                  </p>
+                </blockquote>
+                <figcaption className='mt-10 flex items-center gap-x-6'>
+                  <img
+                    className='h-14 w-14 rounded-full bg-gray-50'
+                    src='https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                    alt=''
+                  />
+                  <div className='text-base'>
+                    <div className='font-semibold text-gray-900'>
+                      Joseph Rodriguez
+                    </div>
+                    <div className='mt-1 text-gray-500'>CEO of Reform</div>
+                  </div>
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </div>
+      </section>
+      <GradientCTA
+        headline='Ready to Elevate Your Career?'
+        subheadline='Try a demo, risk-free.'
+        buttonText='Get Started For Free'
+        secondaryButtonText='Need More Info?'
+      />
+    </div>
+  );
 };
 
-export default index;
+export default Index;
