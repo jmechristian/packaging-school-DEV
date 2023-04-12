8a2b7480df5f67fc39cd913bd1595a028ac05360
jmechristian <jmechristian@gmail.com>
2023-04-12 11:18:10 -0400

    sdsdsd

--- components/forms/FormWrapper.js
+++ components/forms/FormWrapper.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useForm, FormProvider, useFormContext } from 'react-hook-form';
+import { useForm, FormProvider } from 'react-hook-form';
 
 const FormWrapper = ({ children }) => {
   const methods = useForm();
-  const { formState: errors } = useForm();
   const onSubmit = (data) => console.log(data);
 
   return (
     <div className='mx-auto w-full px-16 pt-6 pb-12 bg-slate-100'>
-      <FormProvider {...methods} {...errors}>
+      <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
       </FormProvider>
     </div>
