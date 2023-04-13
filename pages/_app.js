7b431fa8fdfde8d9cd7d0dd7e096a2a92f9ba410
jmechristian <jmechristian@gmail.com>
2023-04-13 09:40:14 -0400

    sssss

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import '../styles/globals.css';
 import Layout from '../features/layout/Layout';
-import { Analytics } from 'aws-amplify';
 import Link from 'next/link';
-import algoliasearch from 'algoliasearch/lite';
 import { PrismicProvider } from '@prismicio/react';
 import { PrismicPreview } from '@prismicio/next';
 import { linkResolver, repositoryName } from '../prismicio';
@@ -10,23 +8,6 @@ import { store } from '../features/store';
 import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }) {
-  const searchClient = algoliasearch(
-    'W0TY58JH2W',
-    '0014bb643f8b92e979f2e8c81e961ca6'
-  );
-
-  const analyticsConfig = {
-    AWSPinpoint: {
-      // Amazon Pinpoint App Client ID
-      appId: '196d27043c7a4001aa96a55edb90d78e',
-      // Amazon service region
-      region: 'us-east-1',
-      mandatorySignIn: false,
-    },
-  };
-
-  Analytics.configure(analyticsConfig);
-
   return (
     <PrismicProvider
       linkResolver={linkResolver}
