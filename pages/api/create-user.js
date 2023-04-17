0c06c6aa6b16468d573d2dc72a7686e18e0c9b37
jmechristian <jmechristian@gmail.com>
2023-04-17 10:32:53 -0400

    ddd

--- pages/api/create-user.js
+++ pages/api/create-user.js
@@ -1,10 +1,19 @@
-import { PutCommand } from '@aws-sdk/lib-dynamodb';
+import { PutCommand, QueryCommand } from '@aws-sdk/lib-dynamodb';
 import { ddbDocClient } from '../../libs/ddbDocClient';
+
+const command = new QueryCommand({
+  TableName: 'User-keuzpzdyrrd5bktwodatklekcu-staging',
+  IndexName: 'byEmail',
+  KeyConditionExpression: 'email = :email',
+  ExpressionAttributeValues: {
+    ':email': { S: email },
+  },
+});
 
 export default async function createUser(req, res) {
   if (req.method === 'POST') {
-    res.status(201);
-    res.json({ message: 'Yes' });
+    const isUser = res.status(201);
+    res.json({ user: req.body });
   } else {
     res.status(402);
     res.json({ message: 'No' });
