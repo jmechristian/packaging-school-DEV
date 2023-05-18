05078d3c41e90599b7768705e6c667227f537464
jmechristian <jmechristian@gmail.com>
2023-05-18 16:39:23 -0400

    sss

---
+++ components/nav/CertMenuItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CertMenuItem = () => {
+  return (
+    <div>CertMenuItem</div>
+  )
+}
+
+export default CertMenuItem
--- components/nav/CoursesMenuItem.js
+++ components/nav/CoursesMenuItem.js
@@ -1,52 +1,53 @@
-import React from 'react'
+import React from 'react';
 
 const CoursesMenuItem = () => {
   return (
-    {navigation.categories.map((category, categoryIdx) => (
-        <Popover key={category.name} className='flex'>
-          {({ open }) => (
-            <>
-              <div className='relative flex'>
-                <Popover.Button
-                  className={classNames(
-                    open
-                      ? 'border-clemson text-clemson'
-                      : 'border-transparent text-gray-700 hover:text-gray-800',
-                    'relative z-10 -mb-px flex items-center border-b-2 pt-px text-sm font-semibold transition-colors duration-200 ease-out'
-                  )}
-                >
-                  {category.name}
-                </Popover.Button>
-              </div>
+    // {navigation.categories.map((category, categoryIdx) => (
+    //     <Popover key={category.name} className='flex'>
+    //       {({ open }) => (
+    //         <>
+    //           <div className='relative flex'>
+    //             <Popover.Button
+    //               className={classNames(
+    //                 open
+    //                   ? 'border-clemson text-clemson'
+    //                   : 'border-transparent text-gray-700 hover:text-gray-800',
+    //                 'relative z-10 -mb-px flex items-center border-b-2 pt-px text-sm font-semibold transition-colors duration-200 ease-out'
+    //               )}
+    //             >
+    //               {category.name}
+    //             </Popover.Button>
+    //           </div>
 
-              <Transition
-                as={Fragment}
-                enter='transition ease-out duration-200'
-                enterFrom='opacity-0'
-                enterTo='opacity-100'
-                leave='transition ease-in duration-150'
-                leaveFrom='opacity-100'
-                leaveTo='opacity-0'
-              >
-                <Popover.Panel className='absolute inset-x-0 z-30 top-full bg-white text-gray-500 sm:text-sm'>
-                  {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-                  <div
-                    className='absolute inset-0 top-1/2 bg-white shadow'
-                    aria-hidden='true'
-                  />
+    //           <Transition
+    //             as={Fragment}
+    //             enter='transition ease-out duration-200'
+    //             enterFrom='opacity-0'
+    //             enterTo='opacity-100'
+    //             leave='transition ease-in duration-150'
+    //             leaveFrom='opacity-100'
+    //             leaveTo='opacity-0'
+    //           >
+    //             <Popover.Panel className='absolute inset-x-0 z-30 top-full bg-white text-gray-500 sm:text-sm'>
+    //               {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
+    //               <div
+    //                 className='absolute inset-0 top-1/2 bg-white shadow'
+    //                 aria-hidden='true'
+    //               />
 
-                  <div className='relative bg-white'>
-                    <div className='mx-auto max-w-7xl px-8'>
-                      Why
-                    </div>
-                  </div>
-                </Popover.Panel>
-              </Transition>
-            </>
-          )}
-        </Popover>
-      ))}
-  )
-}
+    //               <div className='relative bg-white'>
+    //                 <div className='mx-auto max-w-7xl px-8'>
+    //                   Why
+    //                 </div>
+    //               </div>
+    //             </Popover.Panel>
+    //           </Transition>
+    //         </>
+    //       )}
+    //     </Popover>
+    //   ))}
+    <div></div>
+  );
+};
 
-export default CoursesMenuItem
+export default CoursesMenuItem;
