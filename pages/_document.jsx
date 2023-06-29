bfee762e662c50d010019e3ff826d4bb5f314959
jmechristian <jmechristian@gmail.com>
2023-06-29 15:37:01 -0400

    ssss

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -9,6 +10,20 @@ export default function Document() {
     >
       <Head>
         <link rel='stylesheet' href='https://use.typekit.net/qtr6hem.css' />
+        <Script
+          id='hotjar'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3338111,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+        />
       </Head>
       <body className='flex flex-col min-h-screen'>
         <Main />
