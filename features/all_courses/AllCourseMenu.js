0ec0abc659e96068f2b9c29b04f395f7e3979020
jmechristian <jmechristian@gmail.com>
2023-07-15 21:21:03 -0500

    iiiii

--- features/all_courses/AllCourseMenu.js
+++ features/all_courses/AllCourseMenu.js
@@ -19,26 +19,6 @@ const AllCourseMenu = () => {
     <section className='flex flex-col gap-8 h-full mt-1'>
       <div className='flex flex-col gap-6 lg:gap-8'>
         <div className='flex flex-col gap-2'>
-          <div className='text-slate-900 dark:text-clemson font-semibold font-greycliff text-xl lg:text-lg'>
-            Certificate Programs
-          </div>
-          <div className='flex flex-col'>
-            {certMenu.map((item, i) => (
-              <div
-                className={`${
-                  selectedFilter.value === item.value
-                    ? 'bg-base-brand text-slate-200 font-medium rounded'
-                    : 'text-slate-500 dark:text-gray-400'
-                } pl-2 py-1.5 cursor-pointer`}
-                key={item.value}
-                onClick={() => filterClickHandler(item.name, item.value)}
-              >
-                {item.name}
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className='flex flex-col gap-2'>
           <div className='text-slate-900 dark:text-clemson font-greycliff font-semibold text-xl lg:text-lg'>
             Courses By Category
           </div>
@@ -59,7 +39,6 @@ const AllCourseMenu = () => {
           </div>
         </div>
       </div>
-      <AssessmentCTA />
     </section>
   );
 };
