fa79d574a319fc22ab68b62526bc127e7d799b75
jmechristian <jmechristian@gmail.com>
2023-07-18 15:47:45 -0400

    sssss

--- components/shared/CTAButton.js
+++ components/shared/CTAButton.js
@@ -7,7 +7,7 @@ const CTAButton = ({ text, click }) => {
     <FadeIn>
       <div className='flex flex-col md:flex-row items-center gap-6 mt-4'>
         <button
-          className='w-full md:w-fit px-9 bg-clemson rounded-lg py-4 text-white font-semibold font-greycliff text-xl'
+          className='w-full md:w-fit px-9 bg-clemson rounded-lg py-4 text-white font-semibold font-greycliff leading-tight text-lg lg:text-xl'
           onClick={click}
         >
           {text}
--- components/shared/SignInModal.js
+++ components/shared/SignInModal.js
@@ -38,10 +38,10 @@ export default function SignInModal({ open, setOpen }) {
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl sm:p-9'>
-                <div className='flex gap-16'>
+              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white px-5 pb-6 pt-6 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl sm:p-9'>
+                <div className='flex flex-col lg:flex-row gap-6 lg:gap-12 text-center lg:text-left'>
                   <div
-                    className='aspect-[3/4] w-full bg-black rounded-xl bg-cover bg-center'
+                    className='aspect-[4/2] lg:aspect-[3/4] w-full bg-black rounded-xl bg-cover bg-center'
                     style={{
                       backgroundImage: `url('https://packschool.s3.amazonaws.com/signin_mock.webp')`,
                     }}
@@ -49,7 +49,7 @@ export default function SignInModal({ open, setOpen }) {
                   <div className='flex flex-col gap-3'>
                     <Dialog.Title
                       as='h2'
-                      className='text-3xl xl:text-4xl font-greycliff font-semibold leading-6 text-gray-900'
+                      className='text-2xl xl:text-4xl font-greycliff font-semibold leading-6 text-gray-900'
                     >
                       <h2>
                         All that knowledge. <br />
@@ -57,7 +57,7 @@ export default function SignInModal({ open, setOpen }) {
                       </h2>
                     </Dialog.Title>
                     <div>
-                      <p className='text-lg text-gray-500'>
+                      <p className='lg:text-lg text-gray-500 leading-snug'>
                         Access all your favorited Packaging School content
                         including courses, articles, saved applications, and
                         more all in one convenient place. Use your LinkedIn or
@@ -70,7 +70,7 @@ export default function SignInModal({ open, setOpen }) {
                         click={signInHandler}
                       />
                     </div>
-                    <div className='text-sm text-gray-600 mt-2'>
+                    <div className='text-sm text-center lg:text-left text-gray-600 mt-2'>
                       Issues logging in? Email{' '}
                       <a href='mailto:info@packagingschool.com'>
                         info@packagingschool.com.
