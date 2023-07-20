c1bf1c3aacb25aed231adaf98f962053922ed6c9
jmechristian <jmechristian@gmail.com>
2023-07-20 16:44:30 -0400

    cvcvcvv

---
+++ pages/api/send-suggs-email.js
@@ -0,0 +1,62 @@
+import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
+const REGION = 'us-east-1';
+const creds = {
+  accessKeyId: process.env.AWSACCESSKEYID,
+  secretAccessKey: process.env.AWSSECRETACCESSKEY,
+};
+// Create SES service object.
+const sesClient = new SESClient({ region: REGION, credentials: creds });
+export { sesClient };
+
+export default async function handler(req, res) {
+  const body = req.body;
+
+  const createSendEmailCommand = (toAddress, fromAddress) => {
+    return new SendEmailCommand({
+      Destination: {
+        /* required */
+        CcAddresses: [
+          /* more items */
+        ],
+        ToAddresses: [
+          toAddress,
+          /* more To-email addresses */
+        ],
+      },
+      Message: {
+        /* required */
+        Body: {
+          /* required */
+          Html: {
+            Data: 'Data',
+          },
+          Text: {
+            Charset: 'UTF-8',
+            Data: 'TEXT_FORMAT_BODY',
+          },
+        },
+        Subject: {
+          Charset: 'UTF-8',
+          Data: 'APS Speaker Profile',
+        },
+      },
+      Source: fromAddress,
+      ReplyToAddresses: [
+        /* more items */
+      ],
+    });
+  };
+
+  try {
+    await sesClient.send(
+      createSendEmailCommand(
+        'jamie@packagingschool.com',
+        'jamie@packagingschool.com'
+      )
+    );
+    res.status(200).json({ message: 'Success' + res });
+  } catch (error) {
+    console.log(error);
+    res.status(410).json({ message: error + 'error' });
+  }
+}
