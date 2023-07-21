147d04fa7a58aca73479e126173bbac1b5178231
jmechristian <jmechristian@gmail.com>
2023-07-21 14:24:39 -0400

    ssss

--- pages/certifications/index.js
+++ pages/certifications/index.js
@@ -48,61 +48,57 @@ const Index = () => {
           <div className='mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
             <div className='flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20'>
               <img
-                className='h-12 self-start'
-                src='https://tailwindui.com/img/logos/tuple-logo-gray-900.svg'
+                className='h-20 self-start'
+                src='https://packschool.s3.amazonaws.com/skg_logo.png'
                 alt=''
               />
               <figure className='mt-10 flex flex-auto flex-col justify-between'>
                 <blockquote className='text-lg leading-8 text-gray-900'>
                   <p>
-                    “Amet amet eget scelerisque tellus sit neque faucibus non
-                    eleifend. Integer eu praesent at a. Ornare arcu gravida
-                    natoque erat et cursus tortor consequat at. Vulputate
-                    gravida sociis enim nullam ultricies habitant malesuada
-                    lorem ac. Tincidunt urna dui pellentesque sagittis.”
+                    “The CMPM is a very complete and inspiring program, its
+                    content has contributed to reinforce my knowledge in key
+                    areas such as materials, production processes, and supply
+                    chain. This allowed me to keep growing as a packaging
+                    professional and will further contribute to strengthen my
+                    value proposition and packaging solutions to my customers”
                   </p>
                 </blockquote>
                 <figcaption className='mt-10 flex items-center gap-x-6'>
-                  <img
-                    className='h-14 w-14 rounded-full bg-gray-50'
-                    src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                    alt=''
-                  />
                   <div className='text-base'>
                     <div className='font-semibold text-gray-900'>
-                      Judith Black
+                      Carolina Aristizábal Molina
                     </div>
-                    <div className='mt-1 text-gray-500'>CEO of Tuple</div>
+                    <div className='mt-1 text-gray-500'>
+                      Packaging Engineer, Smurfit Kappa
+                    </div>
                   </div>
                 </figcaption>
               </figure>
             </div>
             <div className='flex flex-col border-t border-gray-900/10 pt-10 sm:pt-16 lg:border-t-0 lg:border-l lg:pt-0 lg:pl-8 xl:pl-20'>
               <img
-                className='h-12 self-start'
-                src='https://tailwindui.com/img/logos/reform-logo-gray-900.svg'
+                className='h-20 self-start'
+                src='https://packschool.s3.amazonaws.com/colgate.png'
                 alt=''
               />
               <figure className='mt-10 flex flex-auto flex-col justify-between'>
                 <blockquote className='text-lg leading-8 text-gray-900'>
                   <p>
-                    “Excepteur veniam labore ullamco eiusmod. Pariatur consequat
-                    proident duis dolore nulla veniam reprehenderit nisi officia
-                    voluptate incididunt exercitation exercitation elit. Nostrud
-                    veniam sint dolor nisi ullamco.”
+                    “The CMPM is a great program for both up-and-coming
+                    professionals curious to know more about packaging
+                    engineering as well as seasoned packaging professionals who
+                    are looking to do deeper dives and obtain additional
+                    knowledge; very fun, useful and engaging overall!”
                   </p>
                 </blockquote>
                 <figcaption className='mt-10 flex items-center gap-x-6'>
-                  <img
-                    className='h-14 w-14 rounded-full bg-gray-50'
-                    src='https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                    alt=''
-                  />
                   <div className='text-base'>
                     <div className='font-semibold text-gray-900'>
-                      Joseph Rodriguez
+                      Mario Barrios
                     </div>
-                    <div className='mt-1 text-gray-500'>CEO of Reform</div>
+                    <div className='mt-1 text-gray-500'>
+                      Senior Packaging Engineer, Colgate-Palmolive Company
+                    </div>
                   </div>
                 </figcaption>
               </figure>
