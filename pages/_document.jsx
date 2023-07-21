c8f52819d95267a948d37be929fa3b03c99051ce
jmechristian <jmechristian@gmail.com>
2023-07-21 14:56:43 -0400

    DDDD

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -24,10 +24,11 @@ export default function Document() {
           id='google-analytics'
           strategy='afterInteractive'
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-02DBGHP71V');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5H8KXB9');`,
           }}
         />
         <link
@@ -52,6 +53,11 @@ export default function Document() {
       <body className='flex flex-col min-h-screen' id='home'>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5H8KXB9" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
       </body>
     </Html>
   );
