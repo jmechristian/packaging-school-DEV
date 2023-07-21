34198eeab13545dad16b116f39d95fb84804d6a3
jmechristian <jmechristian@gmail.com>
2023-07-21 09:47:29 -0400

    ssss

--- components/home/HomeTestimonials.js
+++ components/home/HomeTestimonials.js
@@ -1,35 +1,42 @@
 const featuredTestimonial = {
-  body: 'Integer id nunc sit semper purus. Bibendum at lacus ut arcu blandit montes vitae auctor libero. Hac condimentum dignissim nibh vulputate ut nunc. Amet nibh orci mi venenatis blandit vel et proin. Non hendrerit in vel ac diam.',
+  body: 'The CMPM program offers valuable insights into the latest packaging trends and processes.  It also provides an excellent framework for project management. I highly encourage anyone who is interested in taking their packaging knowledge to the next level to take this course.',
   author: {
-    name: 'Brenna Goyette',
-    handle: 'brennagoyette',
+    name: 'Clay Mitchell',
+    handle: 'Category Manager, American Paper and Twine',
     imageUrl:
-      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80',
-    logoUrl: 'https://tailwindui.com/img/logos/savvycal-logo-gray-900.svg',
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
 };
 const testimonials = [
   [
     [
       {
-        body: 'Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.',
+        body: 'The CMPM is a great program for both up-and-coming professionals curious to know more about packaging engineering as well as seasoned packaging professionals who are looking to do deeper dives and obtain additional knowledge; very fun, useful and engaging overall!',
         author: {
-          name: 'Leslie Alexander',
-          handle: 'lesliealexander',
+          name: 'Mario Barrios',
+          handle: 'Senior Packaging Engineer, Colgate-Palmolive Company',
           imageUrl:
             'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         },
       },
-      // More testimonials...
+      {
+        body: 'At this point in my career, I was searching for an educational opportunity with the most comprehensive, data driven, and practical program and the CMPM offered that in droves. I was able to use my background to traverse other packaging disciplines and learn all new platforms I had interacted with but never had the chance to dive in. I recommend this program whether you’re just starting in packaging or even with several years under your belt.',
+        author: {
+          name: 'Adam Moffitt',
+          handle: 'Business Development Manager, UPM Raflatac',
+          imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+      },
     ],
     [
       {
-        body: 'Aut reprehenderit voluptatem eum asperiores beatae id. Iure molestiae ipsam ut officia rem nulla blanditiis.',
+        body: 'This program met my expectations. In this program we are learning more in-depth compared to what we could learn in school and at a much more accelerated pace. For the price and going through the program, this program met my objective.',
         author: {
-          name: 'Lindsay Walton',
-          handle: 'lindsaywalton',
+          name: 'Gavan Singh',
+          handle: 'Packaging Eng and Logistic Coordinator Draexlmaier',
           imageUrl:
-            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         },
       },
       // More testimonials...
@@ -38,24 +45,33 @@ const testimonials = [
   [
     [
       {
-        body: 'Voluptas quos itaque ipsam in voluptatem est. Iste eos blanditiis repudiandae. Earum deserunt enim molestiae ipsum perferendis recusandae saepe corrupti.',
+        body: 'I loved the fact that the CMPM program was an interactive online series that inspired my creativity and enabled my ability to use new tools to take back to Nissan North America, Inc. It allowed me to implement an effective and efficient packaging process to help not only me, but my colleagues in their everyday performance on the job.',
         author: {
-          name: 'Tom Cook',
-          handle: 'tomcook',
+          name: 'Lauren McDonald',
+          handle: 'Packaging Engineer II, Nissan Motor Corporation',
           imageUrl:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         },
       },
       // More testimonials...
     ],
     [
       {
-        body: 'Molestias ea earum quos nostrum doloremque sed. Quaerat quasi aut velit incidunt excepturi rerum voluptatem minus harum.',
+        body: 'The CMPM is a very complete and inspiring program, its content  has contributed to reinforce my knowledge in key areas such as materials, production processes, and supply chain. This allowed me to keep growing as a packaging professional and will further contribute to strengthen my value proposition and packaging solutions to my customers.',
         author: {
-          name: 'Leonard Krasner',
-          handle: 'leonardkrasner',
+          name: 'Carolina Aristizábal Molina',
+          handle: 'Packaging Engineer, Smurfit Kappa',
           imageUrl:
-            'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+      },
+      {
+        body: 'The CMPM was a constructive overview of all things packaging! The coursework provided was informational and applicable to everyday practices within our organization; laying the groundwork for what it takes to launch a package.',
+        author: {
+          name: 'Garrett Patrick',
+          handle: 'Packaging Engineer, Gorilla Glue',
+          imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         },
       },
       // More testimonials...
@@ -109,22 +125,12 @@ export default function HomeTestimonials() {
               <p>{`“${featuredTestimonial.body}”`}</p>
             </blockquote>
             <figcaption className='flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4'>
-              <img
-                className='h-10 w-10 flex-none rounded-full bg-gray-50'
-                src={featuredTestimonial.author.imageUrl}
-                alt=''
-              />
               <div className='flex-auto'>
                 <div className='font-semibold'>
                   {featuredTestimonial.author.name}
                 </div>
-                <div className='text-gray-600'>{`@${featuredTestimonial.author.handle}`}</div>
+                <div className='text-gray-600'>{`${featuredTestimonial.author.handle}`}</div>
               </div>
-              <img
-                className='h-10 w-auto flex-none'
-                src={featuredTestimonial.author.logoUrl}
-                alt=''
-              />
             </figcaption>
           </figure>
           {testimonials.map((columnGroup, columnGroupIdx) => (
@@ -153,16 +159,11 @@ export default function HomeTestimonials() {
                         <p>{`“${testimonial.body}”`}</p>
                       </blockquote>
                       <figcaption className='mt-6 flex items-center gap-x-4'>
-                        <img
-                          className='h-10 w-10 rounded-full bg-gray-50'
-                          src={testimonial.author.imageUrl}
-                          alt=''
-                        />
                         <div>
                           <div className='font-semibold'>
                             {testimonial.author.name}
                           </div>
-                          <div className='text-gray-600'>{`@${testimonial.author.handle}`}</div>
+                          <div className='text-gray-600'>{`${testimonial.author.handle}`}</div>
                         </div>
                       </figcaption>
                     </figure>
