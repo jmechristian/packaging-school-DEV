4093dbe9211af5c0eb8f49dc538dd3bce4e71b06
jmechristian <jmechristian@gmail.com>
2023-07-24 11:00:16 -0400

    qqqqq

--- components/shared/CTAButton.js
+++ components/shared/CTAButton.js
@@ -7,7 +7,7 @@ const CTAButton = ({ text, click }) => {
     <FadeIn>
       <div className='flex flex-col md:flex-row items-center gap-6 mt-4 w-full'>
         <button
-          className='w-full md:w-fit px-9 bg-clemson rounded-lg py-4 text-white font-semibold font-greycliff leading-tight lg:text-lg'
+          className='w-full px-9 bg-clemson rounded-lg py-4 text-white font-semibold font-greycliff leading-tight lg:text-lg'
           onClick={click}
         >
           {text}
