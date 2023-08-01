4a6e948a88ef4a1b7129a3c61e93d140f949d5d5
jmechristian <jmechristian@gmail.com>
2023-08-01 11:06:39 -0400

    ssss

---
+++ pages/testimonials.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeTestimonials from '../components/home/HomeTestimonials';
+
+const Page = () => {
+  return (
+    <div className='py-24'>
+      <HomeTestimonials />
+    </div>
+  );
+};
+
+export default Page;
