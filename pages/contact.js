11e0d8dd3b6a75a3b561202a4e447dbe73eb3d87
jmechristian <jmechristian@gmail.com>
2023-08-01 09:07:07 -0400

    ssss

--- pages/contact.js
+++ pages/contact.js
@@ -12,6 +12,7 @@
   }
   ```
 */
+import { useState } from 'react';
 import {
   BuildingOffice2Icon,
   EnvelopeIcon,
@@ -26,7 +27,26 @@ export default function Example() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => sendSubmitNotification(data);
+
+  const [isSending, setIsSending] = useState(false);
+  const [hasSent, setHasSent] = useState(false);
+
+  const sendSubmitNotification = async (data) => {
+    setIsSending(true);
+    await fetch('/api/send-contact-email', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...data,
+      }),
+    });
+    setIsSending(false);
+    setHasSent(true);
+  };
 
   return (
     <div className='relative isolate bg-white'>
@@ -135,7 +155,7 @@ export default function Example() {
               </div>
               <div>
                 <label
-                  htmlFor='last-name'
+                  htmlFor='lastName'
                   className='block text-sm font-semibold leading-6 text-gray-900'
                 >
                   Last name
@@ -143,10 +163,11 @@ export default function Example() {
                 <div className='mt-2.5'>
                   <input
                     type='text'
-                    name='last-name'
-                    id='last-name'
+                    name='lastName'
+                    id='lastName'
                     autoComplete='family-name'
                     className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    {...register('lastName', { required: true })}
                   />
                 </div>
               </div>
@@ -164,12 +185,13 @@ export default function Example() {
                     id='email'
                     autoComplete='email'
                     className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    {...register('email', { required: true })}
                   />
                 </div>
               </div>
               <div className='sm:col-span-2'>
                 <label
-                  htmlFor='phone-number'
+                  htmlFor='phone'
                   className='block text-sm font-semibold leading-6 text-gray-900'
                 >
                   Phone number
@@ -177,10 +199,11 @@ export default function Example() {
                 <div className='mt-2.5'>
                   <input
                     type='tel'
-                    name='phone-number'
-                    id='phone-number'
+                    name='phone'
+                    id='phone'
                     autoComplete='tel'
                     className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    {...register('phone')}
                   />
                 </div>
               </div>
@@ -198,6 +221,7 @@ export default function Example() {
                     rows={4}
                     className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                     defaultValue={''}
+                    {...register('message', { required: true })}
                   />
                 </div>
               </div>
@@ -205,9 +229,10 @@ export default function Example() {
             <div className='mt-8 flex justify-end'>
               <button
                 type='submit'
-                className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                className='rounded-md bg-clemson px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-clemson-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clemson'
+                disabled={hasSent}
               >
-                Send message
+                {isSending ? 'Sending...' : hasSent ? 'Sent!' : 'Send Message'}
               </button>
             </div>
           </div>
