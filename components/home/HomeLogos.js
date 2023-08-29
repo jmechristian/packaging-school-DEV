eeb8f2dd9d9624f729e4533961ebf5b8c0958f0c
jmechristian <jmechristian@gmail.com>
2023-08-29 15:38:07 -0400

    ssss

--- components/home/HomeLogos.js
+++ components/home/HomeLogos.js
@@ -48,6 +48,38 @@ export default function HomeLogos() {
           height={400}
         />
       </div>
+      <div className='max-w-prose mx-auto'>
+        <div className='text-center text-xl font-bold text-gray-900 font-greycliff mt-24'>
+          Honored to be{' '}
+          <span className='text-base-brand tracking-normal'>
+            nominated and awarded in two categories for learning advancements{' '}
+          </span>
+          by our multi-year client,{' '}
+          <a
+            href='https://www.colgatepalmolive.com/en-us'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Colgate.
+          </a>
+        </div>
+      </div>
+      <div className='mx-auto mt-12 grid grid-cols-4 items-center gap-x-4 gap-y-10  sm:grid-cols-2 lg:mx-0'>
+        <img
+          className='col-span-2 max-h-40 w-full object-contain lg:col-span-1'
+          src='https://packschool.s3.amazonaws.com/2023_L%26D+Silver.png'
+          alt='Silver Brandon Hall Group Excellence Award'
+          width={400}
+          height={400}
+        />
+        <img
+          className='col-span-2 max-h-40 w-full object-contain lg:col-span-1'
+          src='https://packschool.s3.amazonaws.com/2023_L%26D+Bronze.png'
+          alt='Bronze Brandon Hall Group Excellence Award'
+          width={400}
+          height={400}
+        />
+      </div>
     </div>
   );
 }
