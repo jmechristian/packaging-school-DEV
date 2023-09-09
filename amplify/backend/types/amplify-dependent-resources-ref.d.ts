0b12329419906c131e7873949c45e01924dc9b70
jmechristian <jmechristian@gmail.com>
2023-09-09 14:17:49 -0400

    ssss

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -60,5 +60,11 @@ export type AmplifyDependentResourcesAttributes = {
             "LambdaExecutionRole": "string",
             "LambdaExecutionRoleArn": "string"
         }
+    },
+    "storage": {
+        "pschooladmin": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -34,7 +34,9 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_user_files_s3_bucket": "pschooladmin54032-staging",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
