2ac5b32819ffde0e99f5fb6a7b1c7b36bcfdee90
jmechristian <jmechristian@gmail.com>
2023-10-24 12:49:38 -0400

    dddd

--- pages/pack-design-workshop-for-educators.js
+++ pages/pack-design-workshop-for-educators.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Page = () => {
-  return <div>Page</div>;
+  return <div className='w-full h-full text-center-py-24'>Coming Soon.</div>;
 };
 
 export default Page;
