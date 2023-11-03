37e511049cc753b984d692c1fd345b017ab44194
jmechristian <jmechristian@gmail.com>
2023-11-03 10:33:12 -0400

    sss

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
           line_items: [
             {
               // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-              price: 'price_1O83fPBmL7ZGJcSLz2UOanOn',
+              price: 'price_1O8NjwBmL7ZGJcSLHrfR9bH0',
               quantity: 1,
             },
           ],
