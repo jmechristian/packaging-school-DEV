cfad86674f08266cc89b932ab4fbe7d6aa77a37e
jmechristian <jmechristian@gmail.com>
2023-12-01 15:58:34 -0500

    ssss

--- components/certifications/csp/CSPWhere.js
+++ components/certifications/csp/CSPWhere.js
@@ -6,7 +6,7 @@ import { SectionHeading } from '../../shared/SectionHeading';
 
 const CSPWhere = () => {
   return (
-    <div className='container-7xl flex flex-col gap-9'>
+    <div className='container-7xl flex flex-col gap-9' id='where'>
       <SectionHeading number='3'>Where do I start?</SectionHeading>
       <div className='grid grid-cols-5 gap-16 overflow-hidden'>
         <div className='col-span-3 flex flex-col gap-6'>
@@ -46,7 +46,7 @@ const CSPWhere = () => {
           </div>
         </div>
         <div className='col-span-2 w-full h-full flex flex-col gap-6'>
-          <div className='rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16'>
+          <div className='rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-12'>
             <div className='mx-auto max-w-xs px-8 flex flex-col gap-3'>
               <p className='flex items-baseline justify-center gap-x-2'>
                 <span className='text-5xl font-bold tracking-tight text-gray-900'>
