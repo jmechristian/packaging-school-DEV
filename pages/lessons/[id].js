0538eb6a9134ff3edc1ddfe17baa530a5a959a60
jmechristian <jmechristian@gmail.com>
2023-12-27 21:03:18 -0500

    sss

--- pages/lessons/[id].js
+++ pages/lessons/[id].js
@@ -1,48 +1,53 @@
-import React, { createContext, useState } from 'react';
+import Image from 'next/image';
 import Head from 'next/head';
-import { lessonsBySlug, listLessons } from '../../src/graphql/queries';
+import React, { useMemo } from 'react';
+import { MdTrackChanges, MdFastForward, MdScreenShare } from 'react-icons/md';
 import { API, graphqlOperation } from 'aws-amplify';
-import { useSelector, useDispatch } from 'react-redux';
-import { LockClosedIcon, LockOpenIcon } from '@heroicons/react/24/solid';
+import {
+  ArchiveBoxIcon,
+  AcademicCapIcon,
+  LockClosedIcon,
+  LockOpenIcon,
+} from '@heroicons/react/24/outline';
+import NewCouseCard from '../../components/shared/NewCouseCard';
+import VideoHero from '../../components/lessons/VideoHero';
+import LessonSlides from '../../components/lessons/LessonSlides';
+import ImageHero from '../../components/lessons/ImageHero';
+import WiredLessonCard from '../../components/shared/WiredLessonCard';
+
+import { listLessons } from '../../src/graphql/queries';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleSignInModal } from '../../features/layout/layoutSlice';
 
-import LessonsHeader from '../../components/lessons/LessonsHeader';
-import LessonsMedia from '../../components/lessons/LessonsMedia';
-import LinksButton from '../../components/shared/LinksButton';
-import SocialShare from '../../components/shared/SocialShare';
-import LessonSlides from '../../components/lessons/LessonSlides';
-import LessonHero from '../../components/lessons/LessonHero';
-import RelatedLesson from '../../components/lessons/RelatedLesson';
-import LearningObjectives from '../../components/lessons/LearningObjectives';
-
-export const LessonContext = createContext({
-  unlocked: Boolean,
-  toggleUnlocked: () => {},
-  page: 0,
-  setPageContext: () => {},
-});
-
-// const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT;
-// const GRAPHQL_API_KEY = process.env.GRAPHQL_API_KEY;
-
-const Page = ({ lesson, lessons }) => {
-  const [unlocked, setUnlocked] = useState(false);
-  const [isPage, setIsPage] = useState(0);
-
+const Page = ({ lesson }) => {
+  const newDate = new Date(lesson.updatedAt).toDateString();
   const dispatch = useDispatch();
-
   const { user } = useSelector((state) => state.auth);
+  const { allCourses } = useSelector((state) => state.course_filter);
+
+  const featured = useMemo(() => {
+    const item =
+      allCourses && allCourses.filter((cou) => cou.id === lesson.featured);
+    return item;
+  }, [lesson, allCourses]);
 
   const setMedia = () => {
     switch (lesson.mediaType) {
       case 'IMAGE':
-        return <LessonHero url={lesson.media} />;
+        return (
+          <ImageHero
+            title={lesson.title}
+            date={newDate}
+            authors={lesson.author.items}
+            media={lesson.seoImage}
+          />
+        );
       case 'VIDEO':
-        return <LessonsMedia videoUrl={lesson.media} />;
+        return <VideoHero videoUrl={lesson.media} />;
       case 'SLIDES':
         return <LessonSlides slides={lesson.slides ? lesson.slides : []} />;
       default:
-        return <LessonHero url={lesson.media} />;
+        return <ImageHero url={lesson.media} />;
     }
   };
 
@@ -73,146 +78,193 @@ const Page = ({ lesson, lessons }) => {
           />
           <meta name='description' content={lesson?.subhead} key='desc' />
         </Head>
-        <LessonContext.Provider
-          value={{
-            unlocked: unlocked,
-            toggleUnlocked: () => setUnlocked(true),
-            page: isPage,
-            setPageContext: (val) => setIsPage(val),
-          }}
-        >
-          {/* <div className='flex flex-col gap-12 pt-12 dark:bg-dark-dark'>
-            <LessonsHeader
-              title={lesson.title}
-              subhead={lesson.subhead}
-              id={lesson.id}
-              author={lesson.author.items}
-              date={lesson.updatedAt}
-            />
-            <div className='hidden'>
-              <Image
-                src={lesson?.seoImage}
-                width={200}
-                height={200}
-                alt={lesson?.title}
-              />
-            </div>
-            <div>{lesson && setMedia()}</div>
-            <LessonsContent
-              content={lesson.content}
-              objectives={lesson.objectives}
-            />
-            <RelatedLessons relatedLessons={lessons} lessonId={lesson.id} />
-            <SocialShare
-              title={lesson.title}
-              slug={lesson.slug}
-              subhead={lesson.subhead}
-              type={'lessons'}
-            />
-            {lesson.sources.items ? (
-              <LinksButton sources={lesson.sources.items} />
-            ) : (
-              ''
-            )}
-          </div> */}
-          <main className='flex flex-col gap-24 pt-20 dark:bg-dark-dark bg-white'>
-            <div className='grid lg:grid-cols-2 gap-6 w-full max-w-7xl mx-auto'>
-              <div>{lesson && setMedia()}</div>
-              <LessonsHeader
-                title={lesson.title}
-                subhead={lesson.subhead}
-                id={lesson.id}
-                author={lesson.author.items}
-                date={lesson.updatedAt}
-              />
-            </div>
-            <div
-              className={`w-full flex flex-col lg:grid lg:grid-cols-12 max-w-7xl mx-auto gap-12 px-3`}
-            >
-              <div className='flex flex-col gap-12 col-span-12 lg:col-span-8 xl:pr-12 w-full max-w-prose mx-auto'>
-                {/* <div className='grid grid-cols-12'></div> */}
-                {lesson.objectives && lesson.objectives.length > 0 && (
-                  <div className='border-b border-b-neutral-300 dark:border-b-neutral-500 pb-4'>
-                    <LearningObjectives objectives={lesson.objectives} />
-                  </div>
-                )}
-                {lesson.actionLink && (
-                  <div className='w-full bg-base-dark flex justify-between items-center rounded-lg px-3 py-4'>
-                    <div className='text-white font-semibold'>
-                      {lesson.actionLinkTitle}
-                    </div>
-                    <div
-                      className='w-fit flex gap-1 items-center px-3 py-2 text-white font-bold bg-clemson rounded shadow-lg'
-                      onClick={actionClickHandler}
-                    >
-                      <div>
-                        {user ? (
-                          <LockOpenIcon className='w-5 h-5 fill-white' />
-                        ) : (
-                          <LockClosedIcon className='w-5 h-5 fill-white' />
-                        )}
+        <div className='w-full lg:pt-6 pb-12 relative dark:bg-dark-dark'>
+          <div className='w-full flex flex-col gap-6 lg:gap-9 max-w-6xl mx-auto'>
+            {setMedia()}
+            <div className='w-full flex flex-col lg:flex-row gap-6 lg:gap-12'>
+              <div className='flex w-full'>
+                <div className='flex flex-col gap-6'>
+                  {lesson.mediaType != 'IMAGE' && (
+                    <div className='border-b-8 border-b-neutral-900 dark:border-white pb-9'>
+                      <div className='w-full dark:text-white font-bold max-w-3xl text-3xl lg:text-4xl xl:text-5xl leading-none lg:tracking-tight tracking-tighter px-4 xl:px-0'>
+                        {lesson.title}
                       </div>
-                      <div>Download</div>
                     </div>
-                  </div>
-                )}
-                <div className='relative'>
+                  )}
+                  {lesson.mediaType === 'VIDEO' ? (
+                    <div className='flex flex-col md:flex-row md:items-center lg:gap-2 font-medium border-b border-b-neutral-900 pb-6 px-4 xl:px-0'>
+                      <div className='font-bold text-sm uppercase'>
+                        {newDate}
+                      </div>
+                      {lesson.author.items.length > 0 &&
+                        lesson.author.items.map((a) => (
+                          <div className='text-base-mid' key={a.author.name}>
+                            <a
+                              href={a.author.linkedIn}
+                              rel='noreferrer'
+                              target='_blank'
+                            >
+                              {'/' +
+                                ' ' +
+                                a.author.name +
+                                ',' +
+                                ' ' +
+                                a.author.title}
+                            </a>
+                          </div>
+                        ))}
+                    </div>
+                  ) : lesson.mediaType === 'SLIDES' ? (
+                    <></>
+                  ) : (
+                    <div className='border-t-8 border-t-neutral-900 pt-9'></div>
+                  )}
+                  {lesson.objectives &&
+                  lesson.objectives.length > 0 &&
+                  !lesson.actionLink ? (
+                    <div className='flex flex-col md:flex-row gap-6 w-full bg-brand-yellow/20 dark:bg-base-mid dark:text-white rounded-xl p-9'>
+                      <div className='aspect-[4/3] w-full lg:max-w-[250px] bg-clemson dark:bg-brand-yellow flex items-center justify-center'>
+                        <div className='w-full p-3'>
+                          <Image
+                            src={
+                              'https://packschool.s3.amazonaws.com/LOTM-Logo-Final-White-sm.png'
+                            }
+                            width={600}
+                            height={309}
+                            layout='responsive'
+                            alt='Learning of the Month'
+                          />
+                        </div>
+                      </div>
+
+                      <div className='flex flex-col gap-4'>
+                        <div className='font-bold tracking-tight text-lg lg:text-xl lg:leading-none'>
+                          Learning Objectives
+                        </div>
+                        {lesson.objectives.map((ob) => (
+                          <div className='flex gap-2' key={ob}>
+                            <div>
+                              <MdTrackChanges color='black' size={32} />
+                            </div>
+                            <div className='leading-tight'>{ob}</div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  ) : lesson.objectives && lesson.actionLink ? (
+                    <div className='w-full bg-base-dark flex flex-col gap-4 md:flex-row text-center md:text-left justify-between items-center rounded-lg px-6 py-4'>
+                      <div className='text-white text-lg font-semibold leading-tight'>
+                        {lesson.actionLinkTitle}
+                      </div>
+                      <div
+                        className='w-fit flex gap-1 items-center px-6 py-2 text-white cursor-pointer font-bold bg-clemson rounded shadow-lg'
+                        onClick={actionClickHandler}
+                      >
+                        <div>
+                          {user ? (
+                            <LockOpenIcon className='w-5 h-5 stroke-white' />
+                          ) : (
+                            <LockClosedIcon className='w-5 h-5 stroke-white' />
+                          )}
+                        </div>
+                        <div>Download</div>
+                      </div>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+
                   <div
-                    dangerouslySetInnerHTML={{ __html: lesson.content }}
-                    className='tiptap max-w-prose mx-auto lg:text-lg'
-                  ></div>
-                  {/* <div className='tiptap flex flex-col'>
+                    className={`relative px-6 ${
+                      lesson.mediaType === 'IMAGE' ? 'mt-0' : 'mt-6'
+                    }`}
+                  >
+                    <div
+                      dangerouslySetInnerHTML={{ __html: lesson.content }}
+                      className='tiptap lg:text-lg'
+                    ></div>
+                    {/* <div className='tiptap flex flex-col'>
                 {bodyContent.map((item, i) => (
                   <div key={i}>{bodyCotentHandler(item)}</div>
                 ))}
               </div> */}
+                  </div>
                 </div>
               </div>
 
-              <div className='col-span-12 lg:col-span-4 w-full max-w-prose mx-auto'>
-                <div className='border-4 w-full rounded-lg shadow-sm border-neutral-900 dark:border-neutral-900 py-4 flex flex-col gap-4'>
-                  <div className='font-bold uppercase text-sm dark:text-white px-4'>
-                    Related Lessons
+              {/* Sidebar */}
+              <div className='w-full lg:max-w-[280px] h-full bg-dark-dark dark:bg-dark-mid text-white rounded-xl flex flex-col md:items-start md:grid md:grid-cols-3 lg:flex lg:flex-col gap-6 pt-2 px-6 md:px-0'>
+                <div className='flex flex-col justify-center md:items-start items-center gap-6 p-4 mx-auto'>
+                  <div className='flex flex-col gap-1.5 '>
+                    <div className='flex gap-1 items-center'>
+                      <MdFastForward size={28} color='orange' />
+                      <div className='font-bold tracking-tight text-lg'>
+                        Want to Go Further?
+                      </div>
+                    </div>
+                    <div className='text-sm text-white/60 leading-snug'>
+                      Dive into a comprehensive course crafted by subject-matter
+                      experts.
+                    </div>
                   </div>
-                  <div className='flex flex-col gap-6'>
-                    <RelatedLesson
-                      id={
-                        lesson.related && lesson.related.length > 0
-                          ? lesson.related[0]
-                          : '66a95671-feb8-4d74-8a87-033d71431de8'
-                      }
+                  {featured && featured.length > 0 ? (
+                    <NewCouseCard
+                      title={featured[0].title}
+                      description={featured[0].subheadline}
+                      background={featured[0].seoImage}
+                      link={featured[0].link}
+                      link_text={'Enroll Now'}
+                      Icon={AcademicCapIcon}
+                      video={featured[0].preview}
+                      // id={'806c0e2e-c4db-4c13-94f9-b49d4e8b2239'}
                     />
-                    <RelatedLesson
-                      id={
-                        lesson.related && lesson.related.length > 0
-                          ? lesson.related[1]
-                          : 'e3658e91-82f3-4923-8a58-821ec8d5f0cf'
-                      }
-                    />
-                    <RelatedLesson
-                      id={
-                        lesson.related && lesson.related.length > 0
-                          ? lesson.related[2]
-                          : '29468803-7b8f-48ee-8b04-7619633ab2de'
-                      }
-                    />
+                  ) : (
+                    <>
+                      <NewCouseCard
+                        title={'Packaging Foundations'}
+                        description={
+                          'Learn the intricate dynamics of packaging as a system, integrating roles from marketing to warehousing, to become an effective stakeholder in package design.'
+                        }
+                        background={
+                          'https://packschool.s3.amazonaws.com/packaging-foundations-seoImage-2-sm.png'
+                        }
+                        link={'https://learn.packagingschool.com/enroll/35691'}
+                        link_text={'Enroll Now'}
+                        Icon={AcademicCapIcon}
+                        video={'https://www.youtube.com/watch?v=L4Q6sZlXoe4'}
+                        // id={'806c0e2e-c4db-4c13-94f9-b49d4e8b2239'}
+                      />
+                    </>
+                  )}
+                </div>
+                <div className='flex flex-col justify-center md:items-start items-center md:col-span-2'>
+                  {lesson.related && lesson.related.length > 0 && (
+                    <div className='flex gap-2 items-center px-4 md:py-4'>
+                      <MdScreenShare size={24} color='orange' />
+                      <div className='font-bold tracking-tight text-lg'>
+                        Related Lessons
+                      </div>
+                    </div>
+                  )}
+                  <div className='md:grid md:grid-cols-2 lg:flex lg:flex-col'>
+                    {lesson.related && lesson.related.length > 0 ? (
+                      lesson.related.map((cou) => (
+                        <div
+                          className='border-b border-b-neutral-700 last:border-b-0 pt-3 pb-7 hover:bg-dark-mid transition-colors ease-in px-4 rounded-xl'
+                          key={cou}
+                        >
+                          <WiredLessonCard link_text={'Enroll Now'} id={cou} />
+                        </div>
+                      ))
+                    ) : (
+                      <></>
+                    )}
                   </div>
                 </div>
               </div>
             </div>
-            <SocialShare
-              title={lesson.title}
-              slug={lesson.slug}
-              subhead={lesson.subhead}
-              type={'lessons'}
-            />
-            {lesson.sources.items.length > 0 ? (
-              <LinksButton sources={lesson.sources.items} />
-            ) : (
-              ''
-            )}
-          </main>
-        </LessonContext.Provider>
+          </div>
+        </div>
       </>
     )
   );
@@ -284,6 +336,7 @@ export async function getStaticProps({ params }) {
             }
           }
           title
+          featured
           related
           type
           updatedAt
@@ -299,13 +352,10 @@ export async function getStaticProps({ params }) {
     slug: id, // key is "input" based on the mutation above
   };
 
-  const res = await API.graphql(graphqlOperation(getLesson, variables));
+  const res = await API.graphql({ query: getLesson, variables: variables });
   const lesson = res.data.lessonsBySlug.items[0];
 
-  const getLessons = await API.graphql({ query: listLessons });
-  const lessons = getLessons.data.listLessons.items;
-
-  return { props: { lesson, lessons }, revalidate: 10 };
+  return { props: { lesson }, revalidate: 10 };
 }
 
 export default Page;
