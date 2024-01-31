6b6fd16816dd69e4bee5e36dd2cc9a7995fb5118
jmechristian <jmechristian@gmail.com>
2024-01-31 14:11:09 -0500

    ssss

--- pages/forms/scholarship-application-form.js
+++ pages/forms/scholarship-application-form.js
@@ -10,7 +10,7 @@ const Index = () => {
       <CenteredTextHero
         subhead='This scholarship application is valid for all courses and programs offered by The Packaging School. You will indicate which course or program you’re applying for in your scholarship essay. The strength of your essay will determine the amount of scholarship you are offered based on the grading rubric provided'
         headline='Scholarship Application Form'
-        heading='Scholarship Decision Dates: March 28, June 27, September 26, December 19.'
+        heading='Scholarship Decision Dates: Scholarship committee reviews applications on a monthly basis.'
         buttonText='View Application'
         buttonLink='https://docs.google.com/forms/d/e/1FAIpQLSfQRoIUCyXDpjN1-aw7-Y7MLbPVBuiDC4sfOfGMPfdI_x5ijQ/viewform'
         secondaryText='Essay Grading Rubric'
