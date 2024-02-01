0bdc144013935d4d86d69b13e3cb4a071b22bb8b
jmechristian <jmechristian@gmail.com>
2024-02-01 13:44:59 -0500

    kjkjkj

--- components/shared/Testimonial.js
+++ components/shared/Testimonial.js
@@ -2,6 +2,7 @@ import React, { useId } from 'react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import FadeIn from '../../helpers/FadeIn';
+import VideoPlayer from '../VideoPlayer';
 
 const styles = {
   xs: 'mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-2',
@@ -40,40 +41,50 @@ const Testimonial = ({ id, author, children }) => {
     <aside
       id={id}
       aria-label={`Testimonial from ${author.name}`}
-      className='relative bg-slate-100 dark:bg-dark-mid py-16 sm:py-32'
+      className='relative bg-slate-200 dark:bg-dark-mid py-16 sm:py-32'
     >
-      <div className='text-slate-900/10 dark:text-gray-800'>
-        <GridPattern x='50%' patternTransform='translate(0 80)' />
-      </div>
       <FadeIn>
-        <Container size='md' className='relative'>
-          <figure>
-            <blockquote className='mt-10 font-display text-2xl lg:text-4xl font-medium tracking-tight text-slate-900 dark:text-white sm:text-center'>
-              {children}
-            </blockquote>
-            <figcaption className='mt-10 flex items-center sm:justify-center'>
-              {author.image && (
-                <div className='overflow-hidden rounded-full h-12 w-12 bg-slate-200'>
-                  <Image
-                    className='h-12 w-12 object-cover'
-                    src={author.image}
-                    alt={author.name}
-                    width={48}
-                    height={48}
-                  />
+        <div className='grid lg:grid-cols-2 gap-16 max-w-7xl md:max-w-2xl lg:max-w-7xl mx-auto px-5 xl:px-0'>
+          <div className='w-full h-full aspect-[16/9] shadow-2xl'>
+            <VideoPlayer
+              videoEmbedLink={
+                'https://youtu.be/opvsR5BB35g?si=bYAV-d_FHEf68Jt8'
+              }
+              light={true}
+            />
+          </div>
+          <Container
+            size='md'
+            className='flex justify-center items-center rounded-2xl border py-10 xl:py-0 border-neutral-300'
+          >
+            <figure>
+              <blockquote className='font-display text-lg md:text-xl xl:text-2xl font-medium tracking-tight text-slate-900 dark:text-white '>
+                {children}
+              </blockquote>
+              <figcaption className='mt-10 flex items-center sm:justify-center'>
+                {author.image && (
+                  <div className='overflow-hidden rounded-full h-12 w-12 bg-slate-200'>
+                    <Image
+                      className='h-12 w-12 object-cover'
+                      src={author.image}
+                      alt={author.name}
+                      width={48}
+                      height={48}
+                    />
+                  </div>
+                )}
+                <div className='ml-4'>
+                  <div className='text-base font-medium leading-6 tracking-tight text-slate-900 dark:text-clemson'>
+                    {author.name}
+                  </div>
+                  <div className='mt-1 text-sm text-slate-600 dark:text-gray-500'>
+                    {author.role}
+                  </div>
                 </div>
-              )}
-              <div className='ml-4'>
-                <div className='text-base font-medium leading-6 tracking-tight text-slate-900 dark:text-clemson'>
-                  {author.name}
-                </div>
-                <div className='mt-1 text-sm text-slate-600 dark:text-gray-500'>
-                  {author.role}
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-        </Container>
+              </figcaption>
+            </figure>
+          </Container>
+        </div>
       </FadeIn>
     </aside>
   );
