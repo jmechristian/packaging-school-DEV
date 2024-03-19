36d105ba0b83a0d5b8a43da69f8248bbb74be4d3
jmechristian <jmechristian@gmail.com>
2024-03-19 10:10:13 -0400

    CMPM deadline updates

--- components/shared/CountdownTimer.js
+++ components/shared/CountdownTimer.js
@@ -2,7 +2,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 
 // NOTE: Change this date to whatever date you want to countdown to :)
-const COUNTDOWN_FROM = '03/11/2024';
+const COUNTDOWN_FROM = '05/02/2024';
 
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
