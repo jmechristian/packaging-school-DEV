fb3f4c68c68c3a9118a548a2e0f1aa5aa4d74362
jmechristian <jmechristian@gmail.com>
2024-06-04 12:32:28 -0400

    footer

--- features/navigation/Footer/FooterLibrary.js
+++ features/navigation/Footer/FooterLibrary.js
@@ -5,23 +5,23 @@ const FooterLibrary = () => {
   return (
     <div className='flex flex-col gap-1 lg:gap-2 text-sm w-full'>
       <div className='font-extrabold text-sm uppercase tracking-widest dark:text-gray-200 font-greycliff'>
-        Library
+        Lessons
       </div>
       <div className='grid grid-cols-2 gap-y-1 gap-x-8 lg:gap-x-4 justify-between'>
         <div className='flex flex-col gap-1'>
           <p className='text-gray-600 dark:text-gray-500 w-fit'>
-            <Link href={'/library'}>Lessons</Link>
+            <Link href={'/lessons'}>Learing of the Month</Link>
           </p>
           <p className='text-gray-600 dark:text-gray-500 w-fit'>
-            <Link href={'/library'}>Articles</Link>
+            <Link href={'/lessons'}>Regulation of the Month</Link>
           </p>
         </div>
         <div className='flex flex-col gap-1'>
           <p className='text-gray-600 dark:text-gray-500 w-fit'>
-            <Link href={'/library'}>Free Tools</Link>
+            <Link href={'/lessons'}>Articles</Link>
           </p>
           <p className='text-gray-600 dark:text-gray-500 w-fit'>
-            <Link href={'/library'}>Glossary</Link>
+            <Link href={'/lessons'}>Glossary</Link>
           </p>
         </div>
       </div>
