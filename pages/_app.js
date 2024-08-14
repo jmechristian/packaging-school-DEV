7e74425bec882735728f440ba7956f152d76ff6e
jmechristian <jmechristian@gmail.com>
2024-08-14 13:09:53 -0400

    s

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
-import { useEffect, useState } from 'react';
 import '../styles/globals.css';
 import Layout from '../features/layout/Layout';
-import Link from 'next/link';
-import { PrismicProvider } from '@prismicio/react';
-import { PrismicPreview } from '@prismicio/next';
-import { linkResolver, repositoryName } from '../prismicio';
 import { store } from '../features/store';
 import { Provider } from 'react-redux';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
-import { createUser } from '../libs/api';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 import awsExports from '../src/aws-exports';
